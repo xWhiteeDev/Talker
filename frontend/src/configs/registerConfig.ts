24bd1ad6d7235e6d03e3f62364e9e76d8b5a0c27
xWhiteeDev <WhiteeDev@proton.me>
2026-06-28 15:21:11 +0200

    Add config folder and config file

---
+++ frontend/src/configs/registerConfig.ts
@@ -0,0 +1,16 @@
+export const registerConfig = {
+    password: {
+        min: 5,
+        max: 200
+    },
+    personalDataRegex: /[^\p{L}\p{N}\s]/u,
+    personalData: {
+        min: 3,
+        max: 15
+    },
+    email: {
+        min: 5,
+        max: 15
+    },
+    minimalUsageAge: 14
+}
