f0f2a9a7d755e2aec1a4d4405be3a714b73aafc4
xWhiteeDev <WhiteeDev@proton.me>
2026-06-30 20:24:37 +0200

    Feat: Complete authRoute

--- backend/routes/authRoute.ts
+++ backend/routes/authRoute.ts
@@ -1,10 +1,48 @@
 import express from "express";
+import { isDataValid } from "../middleware/middleware.js";
+import { authController } from "../loader/dependencyLoader.js";
+import type { IRequirementOptions } from "../interface/middleware/IRequirement.js";
 
 export const authRouter = express.Router()
 
+const cfg: Record<string, IRequirementOptions> = {
+    email: {
+        minLength: 5,
+        maxLength: 35,
+        type: "string",
+        isRequired: true,
+        trimmed: true
+    },
+    password: {
+        minLength: 5,
+        maxLength: 200,
+        type: "string",
+        isRequired: true,
+        trimmed: true
+    },
+    firstName: {
+        minLength: 3,
+        maxLength: 15,
+        type: "string",
+        isRequired: true,
+        trimmed: true
+    },
+    lastName: {
+        minLength: 3,
+        maxLength: 15,
+        type: "string",
+        isRequired: true,
+        trimmed: true
+    },
+    birthdayDate: {
+        minLength: 3,
+        maxLength: 32,
+        type: "string",
+        isRequired: true,
+        trimmed: true
+    }
+}
 
-authRouter.post('/register',(req,res)=>{
-    console.log(req.body)
-    console.log('Register post')
-    //todo
+authRouter.post('/register', isDataValid(cfg), async(req,res)=>{
+   await authController.createUser(req,res)
 })
