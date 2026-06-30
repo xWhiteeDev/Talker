50efdb8cb003dbe797d04d06e978597cee991ab1
xWhiteeDev <WhiteeDev@proton.me>
2026-06-30 21:05:26 +0200

    Feat: Change convention for more developer friendly

--- backend/interface/repository/IAuthRepository.ts
+++ backend/interface/repository/IAuthRepository.ts
@@ -1,10 +1,11 @@
 import type { IAccountRow } from "../database/IAccount.js";
 
 export interface IAuthRepository {
-    findByEmail(email: string): Promise<IAccountRow | null>;
+    findWithCredentials(email: string): Promise<IAccountRow | null>;
     insert( data:IAuthInsertDTO): Promise<boolean>;
     update(data: IAuthUpdateDTO): Promise<boolean>;
-    delete(email:string):Promise<boolean>
+    delete(email:string):Promise<boolean>;
+    isExist(email: string): Promise<boolean>
 }
 export interface IAuthInsertDTO {
     email:string
--- backend/interface/service/IAuthService.ts
+++ backend/interface/service/IAuthService.ts
@@ -2,8 +2,9 @@ import type { IAccountRow } from "../database/IAccount.js";
 import type { IAuthInsertDTO, IAuthUpdateDTO } from "../repository/IAuthRepository.js";
 
 export interface IAuthService {
-    findByEmail(email: string): Promise<IAccountRow | null>;
-    insert(data: IAuthInsertDTO): Promise<boolean>;
-    update(dto: IAuthUpdateDTO): Promise<boolean>
-    delete(email: string): Promise<boolean>
+    findUserWithCredentials(email: string): Promise<IAccountRow | null>;
+    insertUser(document: IAuthInsertDTO): Promise<boolean>;
+    updateUser(document: IAuthUpdateDTO): Promise<boolean>
+    deleteUser(email: string): Promise<boolean>;
+    isUserExist(email: string): Promise<boolean>
 }
