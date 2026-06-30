5a7b608b385c4f96e2e5436a6df613dcfe2d47e5
xWhiteeDev <WhiteeDev@proton.me>
2026-06-30 20:41:22 +0200

    Feat: Add globalMiddleware to future error handling

--- backend/app/server.ts
+++ backend/app/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import type { NextFunction, Request, Response } from 'express'
 import dotEnv from 'dotenv'
 import cors from 'cors'
 import cookieparser from 'cookie-parser'
@@ -10,11 +11,18 @@ const cfg = {
     connectionFaultMessage: process.env['TALKER_SERVER_CONNECTION_FAULT'] ?? '❌ Something went wrong with server connection ❌'
 }
 
+function globalMiddleware(err: Error, req: Request, res: Response, next: NextFunction) {
+    console.error(err);
+    res.status(400).json({ message: err.message })
+}
+
 const app = express()
 app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173'] }))
 app.use(express.json())
 app.use(cookieparser())
 app.use('/api/auth', authRouter)
+app.use(globalMiddleware)
+
 
 app.listen(cfg.serverPort, (err) => {
     if (err) {
