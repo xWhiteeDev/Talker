1260f41ac412f0b433059ecbbccb26ca7a980e2e
xWhiteeDev <WhiteeDev@proton.me>
2026-06-30 20:20:35 +0200

    Chore: Add middleware

---
+++ backend/middleware/middleware.ts
@@ -0,0 +1,59 @@
+import type { NextFunction, Request, Response } from "express";
+import type { IRequirement } from "../interface/middleware/IRequirement.js";
+
+export function isDataValid(requirements: IRequirement) {
+    console.log('Middleware intercepted data!')
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const data = req.body.data
+            if (!data) {
+                throw Error('No data provided')
+            }
+            for (const requirement in requirements) {
+                if (!requirements[requirement]) continue
+                const isRequired = requirements[requirement].isRequired
+                if (!data[requirement] && isRequired) {
+                    throw new Error(`Missing property in provided data! ${requirement}`);
+                }
+                if (!data[requirement] && !isRequired) {
+                    console.warn(`Requirement is not present but not required ${requirement}`)
+                    continue;
+                }
+                const requiredElement = requirements[requirement]
+                const passedElement = data[requirement]
+                if (requiredElement.type === 'array' && !Array.isArray(passedElement)) {
+                    throw new Error(`Property: ${requirement} do not satisfy Array type.`)
+                }
+                if (typeof passedElement !== requiredElement.type && requiredElement.type !== 'array') {
+                    throw new Error(`Property: ${requirement} do not satisfy type: ${requiredElement.type}`)
+                }
+                if (requiredElement.type === 'object' && (Object.keys(passedElement).length > requiredElement.maxLength || Object.keys(passedElement).length < requiredElement.minLength)) {
+                    throw new Error(`Property: ${requirement} do not satisfy length requirements!`)
+                }
+                if (requiredElement.type === 'array' && passedElement.length > requiredElement.maxLength || passedElement.length < requiredElement.minLength) {
+                    throw new Error(`Property: ${requirement} do not satisfy length requirements!`)
+                }
+                if (requiredElement.type !== 'array' && requiredElement.type !== 'object') {
+                    if (requiredElement.trimmed) {
+                        let value: string
+                        if (typeof passedElement === 'number') {
+                            value = String(passedElement)
+                        } else {
+                            value = passedElement
+                        }
+                        const correctValue = value.trim()
+                        if (correctValue.length > requiredElement.maxLength || correctValue.length < requiredElement.minLength) {
+                            throw new Error(`Property: ${requirement} do not satisfy length requirements!`)
+                        }
+                    }
+                }
+            }
+            next()
+        } catch (error) {
+            next(error)
+
+        }
+
+
+    }
+}
