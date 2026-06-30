f2f71d4cb13e3fe9b39b7202729d13b7a63bc9f8
xWhiteeDev <WhiteeDev@proton.me>
2026-06-30 20:18:32 +0200

    Feat: Add functionality to authController

---
+++ backend/controller/Auth/authController.ts
@@ -0,0 +1,22 @@
+import type { IAuthService } from "../../interface/service/IAuthService.js";
+import type { Request, Response } from "express";
+export class AuthController {
+    constructor(private authService: IAuthService) {
+        console.log(`\x1b[32;1m🚀[AuthController] authService injected \x1b[0m`)
+    }
+
+    async createUser(req: Request, res: Response) {
+        try {
+            const result = await this.authService.insert(req.body.data)
+            if (!result) {
+                res.status(400).json({ message: "Failed to insert result" })
+                return false
+            }
+            res.status(200).json({message:'Record added!'});
+            return true
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ message: 'Internal server error' })
+        }
+    }
+}
