4155bc139a6a272d5ea2fb72581418ffc4bb7420
WhiteeDev <WhiteeDev@proton.me>
2026-07-01 11:33:43 +0200

    Feat: Change convention
    Fixes wrote from phone it will be checked later on pc

--- backend/controller/Auth/authController.ts
+++ backend/controller/Auth/authController.ts
@@ -1,13 +1,13 @@
-import type { IAuthService } from "../../interface/service/IAuthService.js";
+import type { IAccountService } from "../../interface/service/IAuthService.js";
 import type { NextFunction, Request, Response } from "express";
 export class AuthController {
-    constructor(private authService: IAuthService) {
-        console.log(`\x1b[32;1m🚀[AuthController] authService injected \x1b[0m`)
+    constructor(private accountService: IAccountService) {
+        console.log(`\x1b[32;1m🚀[authController] AccountService injected \x1b[0m`)
     }
 
     async createUser(req: Request, res: Response, next: NextFunction) {
         try {
-            const result = await this.authService.insertUser(req.body.data)
+            const result = await this.accountService.insertUser(req.body.data)
             if (!result) {
                 res.status(400).json({ message: "Failed to insert result" })
                 return false
