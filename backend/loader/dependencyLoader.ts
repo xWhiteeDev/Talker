3f9a460f27f862f8d15340f8a8c39f4c113355fb
WhiteeDev <WhiteeDev@proton.me>
2026-07-01 11:37:58 +0200

    Feat: Change convention

--- backend/loader/dependencyLoader.ts
+++ backend/loader/dependencyLoader.ts
@@ -1,10 +1,10 @@
 import { AuthController } from "../controller/Auth/authController.js";
 import { createPool } from "../database/database.js";
-import { AuthRepository } from "../repository/authRepository.js";
-import { AuthService } from "../service/authService.js";
+import { AccountRepository } from "../repository/authRepository.js";
+import { AccountService } from "../service/authService.js";
 
 export const dbPool = createPool();
-const authRepository = new AuthRepository(dbPool)
-const authService = new AuthService(authRepository)
-export const authController = new AuthController(authService)
+const accountRepository = new AccountRepository(dbPool)
+const accountService = new AccountService(accountRepository)
+export const authController = new AuthController(accountService)
 console.log(`\x1b[42;1m✅ All dependencies loaded correctly! \x1b[0m`)
