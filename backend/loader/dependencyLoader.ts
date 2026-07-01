e0ac75d3422baf64fd9a094f0b8dc8338c4b0ed8
xWhiteeDev <WhiteeDev@proton.me>
2026-07-01 21:32:16 +0200

    Feat: Add authController

--- backend/loader/dependencyLoader.ts
+++ backend/loader/dependencyLoader.ts
@@ -1,10 +1,12 @@
 import { AuthController } from "../controller/Auth/authController.js";
 import { createPool } from "../database/database.js";
-import { AccountRepository } from "../repository/authRepository.js";
-import { AccountService } from "../service/authService.js";
+import { AccountRepository } from "../repository/accountRepository.js";
+import { AccountService } from "../service/accountService.js";
+import { AuthService } from "../service/authService.js";
 
 export const dbPool = createPool();
 const accountRepository = new AccountRepository(dbPool)
-const accountService = new AccountService(accountRepository)
-export const authController = new AuthController(accountService)
+const accountService = new AccountService(accountRepository);
+export const authService = new AuthService(accountService)
+export const authController = new AuthController(authService)
 console.log(`\x1b[42;1m✅ All dependencies loaded correctly! \x1b[0m`)
