2d6488ec3f773ef76ff873beefe81ec1034e7c8c
xWhiteeDev <WhiteeDev@proton.me>
2026-07-01 21:30:38 +0200

    Feat: Complete authController, add signIn and signUp handler

--- backend/controller/Auth/authController.ts
+++ backend/controller/Auth/authController.ts
@@ -1,15 +1,14 @@
-import type { IAccountService } from "../../interface/service/IAuthService.js";
 import type { NextFunction, Request, Response } from "express";
-export class AuthController {
-    constructor(private accountService: IAccountService) {
+import type { IAuthService } from "../../interface/service/IAuthService.js";
+import type { IAuthController } from "../../interface/controller/IAuthController.js";
+export class AuthController implements IAuthController {
+    constructor(private authService: IAuthService) {
         console.log(`\x1b[32;1m🚀[authController] AccountService injected \x1b[0m`)
     }
-
-    async createUser(req: Request, res: Response, next: NextFunction) {
+    async createUser(req: Request, res: Response, next: NextFunction): Promise<boolean> {
         try {
-            const result = await this.accountService.insertUser(req.body.data)
+            const result = await this.authService.signUp(req.body.data)
             if (!result) {
-                res.status(400).json({ message: "Failed to insert result" })
                 return false
             }
             res.status(200).json({ message: 'Record added!' });
@@ -17,5 +16,31 @@ export class AuthController {
         } catch (error) {
             next(error)
         }
+        return true
+    }
+    async signIn(req: Request, res: Response, next: NextFunction): Promise<boolean> {
+        try {
+            const data = req.body.data
+            const signResult = await this.authService.signIn({
+                email: data.email,
+                password: data.password
+            });
+            if (!signResult) {
+                next()
+                return false
+            }
+            res.cookie('token', signResult.token, {
+                httpOnly: true,
+                sameSite: true,
+                secure: true
+            })
+            res.status(200).json({ id: signResult.id })
+            return true
+
+        } catch (error) {
+            next(error)
+
+        }
+        return true
     }
 }
