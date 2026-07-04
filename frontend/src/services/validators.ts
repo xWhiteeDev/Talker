eb0f28008ea299798628efaaa948836092791b79
xWhiteeDev <WhiteeDev@proton.me>
2026-07-04 21:02:31 +0200

    Chore: Add more validation methods

--- frontend/src/services/validators.ts
+++ frontend/src/services/validators.ts
@@ -1,4 +1,5 @@
-import type {IValidators} from "../interfaces/routes/Auth/IAuth.ts"
+import type { IConfigLength, IValidators } from "../interfaces/helpers/IValidationHelpers"
+
 export function validateLength(value: string, rules: IConfigLength): boolean {
     return value.length <= rules.max && value.length >= rules.min
 }
@@ -13,5 +14,54 @@ export function validateRegex(value: string, rules: RegExp): boolean {
 export const validatorFunctions: IValidators = {
     trim: validateTrim,
     length: validateLength,
-    regex: validateRegex
+    regex: validateRegex,
+    minimalAge: validateAge
+}
+
+export function isDateValid(date: string): boolean {
+    const dateSeparators = ['-', '/', '.'];
+    let separator: string | null = null
+    for (const dateSeparator of dateSeparators) {
+        if (!date.includes(dateSeparator)) continue
+        separator = dateSeparator;
+        break;
+    }
+    if (!separator) return false
+    const [year, month, day]: number[] = date.split(separator).map(Number)
+    if (isNaN(year) || isNaN(month) || isNaN(day)) return false
+    const today = new Date();
+    if (year >= today.getFullYear() + 1 || month > 12 || month < 1 || day < 1) return false
+    const testDate = new Date(year, month - 1, day)
+    if (testDate.getMonth() + 1 !== month || testDate.getDate() !== day) return false;
+    return true
+
+}
+
+export function getPersonAgeByDate(date: string): number | null {
+    const regex = /^\d{4}-\d{2}-\d{2}$/
+    const isDateCorrectFormat = regex.test(date);
+    if (!isDateCorrectFormat) return null
+    const isPassedDateValid = isDateValid(date)
+    if (!isPassedDateValid) return null
+
+    const today = new Date()
+    let correctMonth = today.getMonth() + 1;
+
+    const [birthYear, birthMonth, birthDay] = date.split('-').map(Number);
+
+    let yearlyAge: number = today.getFullYear() - birthYear
+
+    const calculatedBirth = birthMonth * 100 + birthDay;
+    const calculatedToday = correctMonth * 100 + today.getDate();
+    if (calculatedToday >= calculatedBirth) {
+        return yearlyAge
+    } else {
+        return yearlyAge - 1
+    }
+}
+
+export function validateAge(date: string, rule: number): boolean {
+    const personAge = getPersonAgeByDate(date);
+    if (!personAge) return false
+    return personAge >= rule
 }
