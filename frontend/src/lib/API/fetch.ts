ddf0a44093028f68a536464bb8de1e1e6f541b7c
xWhiteeDev <WhiteeDev@proton.me>
2026-07-16 21:42:26 +0200

    Feat: Unify return type from API

--- frontend/src/lib/API/fetch.ts
+++ frontend/src/lib/API/fetch.ts
@@ -1,49 +1,49 @@
-import { isUrlValid } from "../../utils/guards"
-import { ErrorHandler } from "../customError";
-import type { EmitData, EmitMethod } from "./types"
+import {isUrlValid} from "../../utils/guards";
+import {ErrorHandler} from "../customError";
+import type {EmitData, EmitMethod, EmitResult} from "./types";
 
 
 
-export async function emitServer<T>(url: string, method: EmitMethod, data?: EmitData): Promise<T | null> {
+export async function emitServer<T>(url: string, method: EmitMethod, data?: EmitData): Promise<EmitResult<T> | undefined> {
     const fullEndpoint = import.meta.env['VITE_SERVER_ENDPOINT'] + url;
     const isUrlCorrect: boolean = isUrlValid(fullEndpoint);
     if (!isUrlCorrect) {
         throw new ErrorHandler('[emitServer] Provided url is not correct!', 502);
     }
-    if (data && !(Array.isArray(data) || typeof data === 'object' && data !== null)) throw new ErrorHandler('[emitServer] Provided data does not meet requirements!', 500)
+    if (data && !(Array.isArray(data) || typeof data === 'object' && data !== null)) throw new ErrorHandler('[emitServer] Provided data does not meet requirements!', 500);
     const res: Response = await fetch(fullEndpoint, {
         method,
         headers: {
             'Content-type': 'application/json'
         },
-        body: data ? JSON.stringify({ data }) : undefined,
+        body: data ? JSON.stringify({data}) : undefined,
         credentials: 'include'
-    })
+    });
     if (!res.ok) {
-        const responseCode: number = res.status
+        const responseCode: number = res.status;
         try {
-            const responseData: { message: string } = await res.json()
-            const responseMessage: string = responseData.message
-            throw new ErrorHandler(responseMessage, responseCode)
+            const responseData: {message: string;} = await res.json();
+            const responseMessage: string = responseData.message;
+            throw new ErrorHandler(responseMessage, responseCode);
         } catch (error) {
             if (error instanceof ErrorHandler) {
-                throw error
+                throw error;
             }
-            throw new ErrorHandler('Unknown error', responseCode)
+            throw new ErrorHandler('Unknown error', responseCode);
         }
     }
     try {
         if (res.status === 204) {
-            return null
+            return undefined;
         }
-        const responseData: T = await res.json() as T
-        return responseData
+        const responseData: EmitResult<T> = await res.json() as EmitResult<T>;
+        return responseData;
 
     } catch (error) {
         if (error instanceof Error) {
-            throw new ErrorHandler(error.message, 500)
+            throw new ErrorHandler(error.message, 500);
         }
-        throw new ErrorHandler('Unknown error from server', 500)
+        throw new ErrorHandler('Unknown error from server', 500);
     }
 
 }
--- frontend/src/lib/API/types.ts
+++ frontend/src/lib/API/types.ts
@@ -1,7 +1,8 @@
-export type TransmitionResult = TransmitionFault | TransmitionSuccess
 
-type TransmitionFault = { success: false, message: string }
-type TransmitionSuccess = { success: true, data: Record<string, unknown> }
 
-export type EmitMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
-export type EmitData = Record<string,unknown> | []
+export type EmitMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+export type EmitData = Record<string, unknown> | [];
+
+
+
+export type EmitResult<T> = {success: false, code: number, message?: string;} | {success: true, data?: T;};
