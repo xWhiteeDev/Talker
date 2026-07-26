e4b396df2d399f4f975db251771e3202502dcc65
xWhiteeDev <WhiteeDev@proton.me>
2026-07-26 21:59:38 +0200

    Refactor: Change strategy of routing

--- frontend/src/components/Post/Post.tsx
+++ frontend/src/components/Post/Post.tsx
@@ -8,9 +8,10 @@ import { postReactionContext } from "./context/postReactionContext";
 import Reaction from "./Reaction";
 import { NotificationContext } from "../Notification/context/NotificationContext";
 import { ErrorHandler } from "../../lib/customError";
-import Comment from "./comments/Comment";
+import Comment from "../PostDetail/comments/Comment";
 import UserPostInfo from "./extra/User/User";
 import EditableTextArea from "../EditableTextArea/EditableTextArea";
+import {useNavigate, useParams} from "react-router-dom";
 
 const defaultPayload: PostReaction[] = [
   {
@@ -55,8 +56,6 @@ export default function Post({
     ReactionUnion | undefined
   >(undefined);
   const postContent = useRef<HTMLDivElement>(null);
-  const [isExpanded, setExpanded] = useState<boolean>(false);
-  const [comment, setComment] = useState<string>('');
   const [defaultReactions, setDefaultReactions] =
     useState<PostReaction[]>(defaultPayload);
 
@@ -73,6 +72,8 @@ export default function Post({
     setActiveReactionName(activeReaction as ReactionUnion);
   }, []);
 
+
+  const nav = useNavigate()
   const notificationContext = useContext(NotificationContext);
   async function addReaction(name: ReactionUnion): Promise<unknown> {
     if (activeReactionName) {
@@ -148,32 +149,19 @@ export default function Post({
     }
   }
 
-  async function handleAddCommentToPost() {
-    const result = await request<boolean>('/api/postComments', 'POST', {
-      content:comment
-    })
-  }
   return (
-    <div className={isExpanded ? style.expandedContainer : style.container}>
+    <div className={style.container}>
       <UserPostInfo
         avatar={null}
         visibility={visibility}
         authorName={authorName}
         createdAt={createdAt}
       />
-      <div
-        className={
-          isExpanded ? style.expandedContentContainer : style.contentContainer
-        }
-      >
-        <div
-          className={isExpanded ? style.expandedContent : style.content}
-          ref={postContent}
-        >
+      <div className={style.contentContainer}>
+        <div className={style.content} ref={postContent}>
           <span>{content}</span>
         </div>
         {postContent.current &&
-        !isExpanded &&
         postContent.current.scrollHeight > postContent.current.clientHeight ? (
           <div>
             <span
@@ -183,7 +171,9 @@ export default function Post({
                 fontSize: "1.3rem",
                 opacity: "0.7",
               }}
-              onClick={() => setExpanded(true)}
+              onClick={() => {
+                nav(`/post/${id}`);
+              }}
             >
               See more...
             </span>
@@ -217,40 +207,6 @@ export default function Post({
           text="Share"
         />
       </div>
-      {isExpanded && (
-        <div className={style.expandedComments}>
-          <div className={style.expandedCommentCreator}>
-            <EditableTextArea
-              additionalStyle={{
-                height: "auto",
-                fontSize: "1.5rem",
-                width: "36vw",
-              }}
-              placeholder="+ Add your comment"
-              placeholderFontSize="1.4rem"
-              onInput={(text)=>setComment(text)}
-            />
-            <div className={style.expandedCommentBar}>
-              <Button
-                text="Dodaj"
-                isDisabled={comment.length === 0 || comment.length > 200 ? true : false}
-                additionalStyle={{
-                  minWidth: "3.8vw",
-                  aspectRatio: "3/1",
-                  padding: "0.4%",
-                  fontWeight: "600",
-                  background: "#b4d4e0",
-                  borderRadius: "10px",
-                  border: "none",
-                  fontSize: "16px",
-                  boxShadow: "0px 2px 5px #2222223d",
-                }}
-              />
-              <span style={{color:comment.length > 200 ? 'red' : 'black', fontWeight:comment.length > 200 ? '600' : '400'}}>{comment.length}/200</span>
-            </div>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
