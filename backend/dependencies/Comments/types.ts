23b94270a1094d979fbe59e1cc9c5c0c3b3d187d
xWhiteeDev <WhiteeDev@proton.me>
2026-07-26 21:52:22 +0200

    Feat: Add comments type (BETA)

--- backend/dependencies/Comments/types.ts
+++ backend/dependencies/Comments/types.ts
@@ -1,15 +1,57 @@
 import type {RowDataPacket} from "mysql2";
+import type {Request,Response,NextFunction} from "express";
 
-export interface ICommentRepository {
+export interface ICommentsRepository {
     findByPost(postId: number): Promise<CommentRow[] | null>;
-    findByUserId(userId:number): Promise<CommentRow[] | null>
+    findByUserId(userId: number, postId: number): Promise<CommentRow[] | null>;
+    findById(commentId: number): Promise<CommentRow | null>;
+    findByParentId(parentId: number): Promise<CommentRow[] | null>;
+    insertDocument(dto: CommentInsertDTO): Promise<boolean>;
+    updateContent(commentId: number, newContent: string): Promise<boolean>;
+    deleteDocument(commentId: number): Promise<boolean>;
+}
+
+export interface ICommentsService {
+    findCommentsByPost(postId: number): Promise<CommentRow[] | null>;
+    findUserCommentsByPostId(userId: number, postId: number): Promise<CommentRow[] | null>;
+    findCommentByCommentId(commentId: number): Promise<CommentRow | null>;
+    findCommentsByParentId(parentId: number): Promise<CommentRow[] | null>;
+    insertComment(dto: CommentInsertDTO): Promise<boolean>;
+    updateCommentContent(commentId: number, newContent: string): Promise<boolean>;
+    deleteComment(commentId: number): Promise<boolean>;
+}
+
+export interface ICommentsController {
+    insertComment(req:Request,res:Response,next:NextFunction): Promise<boolean>
 }
 
 export interface CommentRow extends RowDataPacket {
     id: number;
     post_id: number;
     user_id: number;
-    parent_id: number;
+    parent_id: number | null;
     content: string;
     created_at: string;
+}
+
+
+export interface CommentInsertDTO {
+    post_id: number;
+    user_id: number;
+    parent_id: number | null;
+    content: string;
+}
+export interface CommentInsertDTO {
+    post_id: number;
+    user_id: number;
+    parent_id: number | null;
+    content: string;
+}
+
+export type AllowedInsertKeys = 'post_id' | 'user_id' | 'parent_id' | 'content';
+
+export interface CommentBody {
+    content:string 
+    postId:number;
+    parentId:number | null
 }
