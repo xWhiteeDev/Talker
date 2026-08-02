1b2241b9a6f7fe30a73719d6f868b57382eb7dc2
xWhiteeDev <WhiteeDev@proton.me>
2026-08-02 22:21:06 +0200

    WIP: Implement first crud operations on commentReaction

--- backend/routes/reaction/reactionRoute.ts
+++ backend/routes/reaction/reactionRoute.ts
@@ -1,14 +1,30 @@
-import express from 'express';
-import {isAccessTokenActive} from '../../middleware/middleware.js';
-import {postReactionController} from '../../loader/dependencyLoader.js';
+import express from "express";
+import { isAccessTokenActive } from "../../middleware/middleware.js";
+import {
+  commentReactionController,
+  postReactionController,
+} from "../../loader/dependencyLoader.js";
 
 export const postReactionRouter = express.Router();
+export const commentReactionRouter = express.Router();
 
-
-postReactionRouter.post('/', isAccessTokenActive(), async (req, res, next) => {
-    await postReactionController.createReaction(req, res, next);
+postReactionRouter.post("/", isAccessTokenActive(), async (req, res, next) => {
+  await postReactionController.createReaction(req, res, next);
 });
 
-postReactionRouter.delete('/', isAccessTokenActive(), async (req, res, next) => {
+postReactionRouter.delete(
+  "/",
+  isAccessTokenActive(),
+  async (req, res, next) => {
     await postReactionController.deleteReaction(req, res, next);
-});
+  },
+);
+//Comment routes
+
+commentReactionRouter.post(
+  "/",
+  isAccessTokenActive(),
+  async (req, res, next) => {
+    await commentReactionController.createReaction(req, res, next);
+  },
+);
