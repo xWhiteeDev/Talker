acb2de052b225e72d597b9d733ecc937abc80ffd
xWhiteeDev <WhiteeDev@proton.me>
2026-08-02 22:20:34 +0200

    Feat: Add CommentReaction layer loader

--- backend/loader/dependencyLoader.ts
+++ backend/loader/dependencyLoader.ts
@@ -1,20 +1,23 @@
-import {AuthController} from "../dependencies/Auth/authController.js";
-import {createPool} from "../database/database.js";
-import {AuthService} from "../dependencies/Auth/authService.js";
-import {AccountRepository} from "../dependencies/Account/accountRepository.js";
-import {AccountService} from "../dependencies/Account/accountService.js";
-import {friendshipRepository} from "../dependencies/Friendship/friendshipRepository.js";
-import {friendshipService} from "../dependencies/Friendship/friendshipService.js";
-import {friendshipController} from "../dependencies/Friendship/friendshipController.js";
-import {PostRepository} from "../dependencies/Post/postRepository.js";
-import {PostService} from "../dependencies/Post/postService.js";
-import {PostController} from "../dependencies/Post/postController.js";
-import {PostReactionRepository} from "../dependencies/PostReaction/postReactionRepository.js";
-import {PostReactionService} from "../dependencies/PostReaction/postReactionService.js";
-import {PostReactionController} from "../dependencies/PostReaction/postReactionController.js";
-import {CommentsRepository} from "../dependencies/Comments/CommentsRepository.js";
-import {CommentsService} from "../dependencies/Comments/CommentsService.js";
-import {CommentsController} from "../dependencies/Comments/CommentsController.js";
+import { AuthController } from "../dependencies/Auth/authController.js";
+import { createPool } from "../database/database.js";
+import { AuthService } from "../dependencies/Auth/authService.js";
+import { AccountRepository } from "../dependencies/Account/accountRepository.js";
+import { AccountService } from "../dependencies/Account/accountService.js";
+import { friendshipRepository } from "../dependencies/Friendship/friendshipRepository.js";
+import { friendshipService } from "../dependencies/Friendship/friendshipService.js";
+import { friendshipController } from "../dependencies/Friendship/friendshipController.js";
+import { PostRepository } from "../dependencies/Post/postRepository.js";
+import { PostService } from "../dependencies/Post/postService.js";
+import { PostController } from "../dependencies/Post/postController.js";
+import { PostReactionRepository } from "../dependencies/PostReaction/postReactionRepository.js";
+import { PostReactionService } from "../dependencies/PostReaction/postReactionService.js";
+import { PostReactionController } from "../dependencies/PostReaction/postReactionController.js";
+import { CommentsRepository } from "../dependencies/Comments/CommentsRepository.js";
+import { CommentsService } from "../dependencies/Comments/CommentsService.js";
+import { CommentsController } from "../dependencies/Comments/CommentsController.js";
+import { CommentReactionRepository } from "../dependencies/CommentReaction/CommentReactionRepository.js";
+import { CommentReactionService } from "../dependencies/CommentReaction/CommentReactionService.js";
+import { CommentReactionController } from "../dependencies/CommentReaction/CommentReactionController.js";
 
 export const dbPool = createPool();
 const accountRepository = new AccountRepository(dbPool);
@@ -25,17 +28,35 @@ const friendsRepository = new friendshipRepository(dbPool);
 const friendsService = new friendshipService(friendsRepository);
 export const friendsController = new friendshipController(friendsService);
 
-
 const postRepository = new PostRepository(dbPool);
-const postService = new PostService(postRepository, friendsService, accountService);
+const postService = new PostService(
+  postRepository,
+  friendsService,
+  accountService,
+);
 export const postController = new PostController(postService);
 
 const postReactionRepository = new PostReactionRepository(dbPool);
 const postReactionService = new PostReactionService(postReactionRepository);
-export const postReactionController = new PostReactionController(postReactionService);
+export const postReactionController = new PostReactionController(
+  postReactionService,
+  postService,
+);
 
 const commentsRepository = new CommentsRepository(dbPool);
 const commentsService = new CommentsService(commentsRepository);
-export const commmentsController = new CommentsController(commentsService);
+export const commmentsController = new CommentsController(
+  commentsService,
+  postService,
+);
+
+const commentReactionRepository = new CommentReactionRepository(dbPool);
+const commentReactionService = new CommentReactionService(
+  commentReactionRepository,
+);
+export const commentReactionController = new CommentReactionController(
+  commentReactionService,
+  postService,
+);
 
 console.log(`\x1b[42;1m✅ All dependencies loaded correctly! \x1b[0m`);
