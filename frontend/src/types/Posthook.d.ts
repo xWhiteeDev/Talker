b447594f9b6eaec68d58686548a989756891928b
xWhiteeDev <WhiteeDev@proton.me>
2026-08-02 22:24:15 +0200

    Fix: Fix missing typings

--- frontend/src/types/Posthook.d.ts
+++ frontend/src/types/Posthook.d.ts
@@ -1,12 +1,16 @@
+import type { ReactionUnion } from './VisualUnions';
+
 export interface ReactionCount {
   [key: string]: number;
 }
 
 export interface Comment {
-  content:string;
-  fullName:string;
-  commentId:number;
-  createdAt:string
+  content: string;
+  fullName: string;
+  commentId: number;
+  createdAt: string;
+  reactions: Record<ReactionUnion, number>;
+  userReaction: ReactionUnion;
 }
 
 export interface PostRow {
