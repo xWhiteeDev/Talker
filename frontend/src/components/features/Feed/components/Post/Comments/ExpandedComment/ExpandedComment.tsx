c7070a5cbd6155f517e8a40d6e4b9c218757dc39
xWhiteeDev <WhiteeDev@proton.me>
2026-08-20 13:28:33 +0200

    Feat: Delete unused

--- frontend/src/components/features/Feed/components/Post/Comments/ExpandedComment/ExpandedComment.tsx
+++
@@ -1,8 +0,0 @@
-import {useParams} from 'react-router-dom';
-import style from './ExpandedComment.module.css'
-
-export function ExpandedComment () {
-    const params = useParams()
-    
-    return <div className={style.expandedCommentContainer}> {params.commentid}</div>
-}
