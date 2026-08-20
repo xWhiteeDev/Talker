672393eee53ed7a55c5c5799e7750ff80a7c22b5
xWhiteeDev <WhiteeDev@proton.me>
2026-08-20 13:26:01 +0200

    Refactor: PostReactions => ActivityReactions

---
+++ frontend/src/components/features/Feed/components/ActivityReactions/ActivityReactions.tsx
@@ -0,0 +1,25 @@
+import { useReaction } from '../../../../../hooks/useReaction';
+import type { ReactionUnion } from '../../../../../types/VisualUnions';
+import Reaction from '../Reaction/Reaction';
+
+interface ActivityReactionsProps {
+  reactions: Record<ReactionUnion, number>;
+  myReaction: ReactionUnion;
+  activityType: 'COMMENT' | 'POST';
+  activityId: number;
+}
+
+export default function ActivityReactions({ reactions, myReaction, activityType, activityId }: ActivityReactionsProps) {
+  const { unifiedReactions, toggle } = useReaction(reactions ?? {}, myReaction);
+  const reactionNames = Object.keys(unifiedReactions.counts) as ReactionUnion[];
+
+  return reactionNames.map((v) => (
+    <Reaction
+      key={v}
+      name={v as ReactionUnion}
+      count={unifiedReactions.counts[v as ReactionUnion]}
+      isActive={unifiedReactions.activeReaction === v}
+      onReactionAdd={(name) => toggle(name as ReactionUnion, activityType, activityId)}
+    />
+  ));
+}
