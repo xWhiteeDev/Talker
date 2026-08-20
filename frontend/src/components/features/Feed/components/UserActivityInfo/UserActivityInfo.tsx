7454181d986421d41aa9e314b2cedde4ee497ead
xWhiteeDev <WhiteeDev@proton.me>
2026-08-20 13:29:25 +0200

    Refactor: UserActivityInfo instead UserInfo

---
+++ frontend/src/components/features/Feed/components/UserActivityInfo/UserActivityInfo.tsx
@@ -0,0 +1,24 @@
+import style from './UserActivityInfo.module.css';
+import unk_person from '../../../../../assets/unk_person.png';
+
+interface UserActivityInfoProps {
+  avatar: string | null;
+  authorName: string;
+  createdAt: string;
+  visibility?: string;
+}
+
+export default function UserActivityInfo({ avatar, authorName, createdAt, visibility }: UserActivityInfoProps) {
+  return (
+    <div className={style.user}>
+      <div className={style.userAvatar}>
+        <img src={avatar ?? unk_person} className={style.avatar} alt="user avatar" />
+      </div>
+      <div className={style.postInfo}>
+        <span style={{ fontWeight: '600' }}>{authorName}</span>
+        {visibility && <span>{visibility}</span>}
+        <span>{new Date(createdAt).toLocaleString()}</span>
+      </div>
+    </div>
+  );
+}
