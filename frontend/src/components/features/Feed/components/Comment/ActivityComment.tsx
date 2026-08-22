08fdfe1a4eeb4283ea983fe869fb4b2f29045e8a
xWhiteeDev <WhiteeDev@proton.me>
2026-08-22 20:28:04 +0200

    Feat: Delete unused style and change padding

--- frontend/src/components/features/Feed/components/Comment/ActivityComment.tsx
+++ frontend/src/components/features/Feed/components/Comment/ActivityComment.tsx
@@ -12,9 +12,9 @@ interface ActivityCommentProps {
   postId: number;
   userReaction: ReactionUnion;
   commentReactions: Record<ReactionUnion, number>;
+  subCommentsCount:number
   onFocus(): void;
 }
-
 
 export function ActivityComment({
   avatar,
@@ -25,17 +25,30 @@ export function ActivityComment({
   postId,
   commentReactions,
   userReaction,
+  subCommentsCount, //TODO
   onFocus,
 }: ActivityCommentProps) {
-  return ( 
+  return (
     <div className={style.activityCommentContainer}>
-      <div className={style.activityCommentContent} onClick={() => onFocus()}>
+      <div className={style.activityUser}>
         <UserActivityInfo avatar={avatar ?? null} authorName={authorName} createdAt={createdAt} />
+      </div>
+      <div className={style.activityCommentContent}>
         <span>{content}</span>
       </div>
       <div className={style.activityCommentReactions}>
-       <ActivityReactions myReaction={userReaction} reactions={commentReactions} activityType='COMMENT' postId={postId} commentId={commentId}/>
+        <ActivityReactions
+          myReaction={userReaction}
+          reactions={commentReactions}
+          activityType="COMMENT"
+          postId={postId}
+          commentId={commentId}
+        />
+      </div>
+      <div className={style.activityCommentCount} onClick={() => onFocus()}>
+        <span>Komentarze: <b>4</b></span>
       </div>
     </div>
   );
 }
+ // TODO!!
