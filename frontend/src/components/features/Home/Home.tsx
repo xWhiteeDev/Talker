85c2b421c9a29ce9096c2e2098a1b4d8b39dad49
xWhiteeDev <WhiteeDev@proton.me>
2026-08-23 17:12:13 +0200

    Feat: Replace Post => Activity

--- frontend/src/components/features/Home/Home.tsx
+++ frontend/src/components/features/Home/Home.tsx
@@ -1,56 +1,54 @@
-import { useContext, useEffect, useState } from "react";
-import style from "./Home.module.css";
-import Logo from "../../generic/UI/Logo/Logo";
-import { useAPI } from "../../../hooks/useAPI";
-import { customNotificationCtx } from "../../../context/customNotificationContext";
-import { usePosts } from "../../../hooks/usePosts";
-import Post from "../Feed/components/Post/Post";
-import { PostCreator } from "../Feed/components/PostCreator/PostCreator";
-import { Loading } from "../../generic/UI/Loading/Loading";
+import { useContext, useEffect, useState } from 'react';
+import style from './Home.module.css';
+import Logo from '../../generic/UI/Logo/Logo';
+import { useAPI } from '../../../hooks/useAPI';
+import { customNotificationCtx } from '../../../context/customNotificationContext';
+import { usePosts } from '../../../hooks/usePosts';
+import { PostCreator } from '../Feed/components/PostCreator/PostCreator';
+import { Loading } from '../../generic/UI/Loading/Loading';
+import { Activity } from '../Feed/components/Activity/Activity';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
   const [postText, setPostText] = useState<string | undefined>(undefined);
   const [visibility, setVisibility] = useState<string | undefined>(undefined);
   const { request } = useAPI();
-  const [isPostIdExpanded, setPostIdExpanded] = useState<number | undefined>(
-    undefined,
-  );
   const { refresh, posts, isLoading } = usePosts();
+  const nav = useNavigate();
   useEffect(() => {
     refresh();
   }, []);
   const ctx = useContext(customNotificationCtx);
-  const filteredItems = posts?.filter((v) => v.id === isPostIdExpanded);
   async function pushPost() {
     if (postText && postText.length === 0) {
       return false;
     }
-    if (!visibility || visibility.trim() === "") {
-      ctx?.setNotify({ type: "error", message: "Select visibility!" });
+    if (!visibility || visibility.trim() === '') {
+      ctx?.setNotify({ type: 'error', message: 'Select visibility!' });
       return false;
     }
-    if (!postText || postText.trim() === "" || postText.length < 4) {
-      ctx?.setNotify({ type: "error", message: "Write more text!" });
+    if (!postText || postText.trim() === '' || postText.length < 4) {
+      ctx?.setNotify({ type: 'error', message: 'Write more text!' });
       return false;
     }
     const payload = {
-      visibleFor: visibility,
+      visible_for: visibility,
       content: postText,
     };
     try {
-      const res = await request<boolean>("/api/posts", "POST", payload);
+      const res = await request<boolean>('/api/posts', 'POST', payload);
       if (res?.success) {
-        ctx?.setNotify({ type: "success", message: "Post added!" });
+        ctx?.setNotify({ type: 'success', message: 'Post added!' });
         refresh();
         setPostText(undefined);
       } else {
-        throw new Error("Post cannot be added");
+        throw new Error('Post cannot be added');
       }
     } catch (error) {
       if (error instanceof Error) {
-        ctx?.setNotify({ type: "error", message: error.message });
+        ctx?.setNotify({ type: 'error', message: error.message });
       } else {
-        ctx?.setNotify({ type: "error", message: "Unknown error!" });
+        ctx?.setNotify({ type: 'error', message: 'Unknown error!' });
       }
     }
   }
@@ -63,55 +61,37 @@ export default function Home() {
       <div className={style.searchbarContainer}></div>
 
       <div className={style.serviceLogoContainer}>
-        <Logo additionalStyle={{ width: "5%" }} />
+        <Logo additionalStyle={{ width: '5%' }} />
       </div>
       <div className={style.profileContainer}></div>
       <div className={style.accountOptions}></div>
-      <div
-        className={
-          isPostIdExpanded ? style.feedContainerExpanded : style.feedContainer
-        }
-      >
-        {!isPostIdExpanded && (
-          <div className={style.addPostContainer}>
-            <PostCreator
-              onWrite={(text: string) => setPostText(text)}
-              text={postText}
-              onOptionChange={(optionChange: string) =>
-                setVisibility(optionChange)
-              }
-              onConfirm={async () => await pushPost()}
-            />
-          </div>
-        )}
-
-        {filteredItems && filteredItems.length !== 0
-          ? filteredItems.map((v) => (
-              <Post
+      <div className={style.feedContainerExpanded}>
+        <div className={style.addPostContainer}>
+          <PostCreator
+            onWrite={(text: string) => setPostText(text)}
+            text={postText}
+            onOptionChange={(optionChange: string) => setVisibility(optionChange)}
+            onConfirm={async () => await pushPost()}
+          />
+        </div>
+        <div className={style.posts}>
+          {posts &&
+            posts.map((v) => (
+              <Activity
+                type="POST"
                 key={v.id}
-                avatar={null} //todo
-                authorName={v.firstName + " " + v.lastName}
+                avatar={null}
+                authorName={v.fullName}
                 content={v.content}
-                visibility={v.visiblefor}
-                createdAt={v.created_at}
-                reactions={v.reactions_object ?? {}}
-                activeReaction={v.myReaction}
-                id={v.id}
-              />
-            ))
-          : posts?.map((v) => (
-              <Post
-                key={v.id}
-                avatar={null} //todo
-                authorName={v.firstName + " " + v.lastName}
-                content={v.content}
-                visibility={v.visiblefor}
-                createdAt={v.created_at}
-                reactions={v.reactions_object ?? {}}
-                activeReaction={v.myReaction}
-                id={v.id}
+                createdAt={v.createdAt}
+                commentReactions={v.reactions}
+                userReaction={v.myReaction}
+                postId={v.id}
+                subCommentsCount={v.commentsCount}
+                onFocus={() => nav(`/post/${v.id}`)}
               />
             ))}
+        </div>
       </div>
     </>
   );
