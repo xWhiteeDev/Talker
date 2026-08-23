e9721ae91c3a5bf81e1230c8756b264f6a5ab370
xWhiteeDev <WhiteeDev@proton.me>
2026-08-23 17:10:53 +0200

    Feat: Rename ActivityComment => Activity

---
+++ frontend/src/components/features/Feed/components/Activity/Activity.tsx
@@ -0,0 +1,58 @@
+import style from './Activity.module.css';
+import type { ReactionUnion } from '../../../../../types/VisualUnions';
+import UserActivityInfo from '../UserActivityInfo/UserActivityInfo';
+import ActivityReactions from '../ActivityReactions/ActivityReactions';
+
+interface ActivityProps {
+  avatar: string | null;
+  authorName: string;
+  content: string;
+  createdAt: string;
+  postId: number;
+  userReaction: ReactionUnion;
+  commentReactions: Record<ReactionUnion, number>;
+  subCommentsCount: number;
+  onFocus(): void;
+  commentId?: number;
+  type:'POST' | 'COMMENT'
+}
+
+export function Activity({
+  type,
+  avatar,
+  authorName,
+  content,
+  createdAt,
+  commentId,
+  postId,
+  commentReactions,
+  userReaction,
+  subCommentsCount, //TODO
+  onFocus,
+}: ActivityProps) {
+  return (
+    <div className={style.activityContainer}>
+      <div className={style.activityUser}>
+        <UserActivityInfo avatar={avatar ?? null} authorName={authorName} createdAt={createdAt} />
+      </div>
+      <div className={style.activityCommentContent}>
+        <span>{content}</span>
+      </div>
+      <div className={style.activityCommentReactions}>
+        <ActivityReactions
+          myReaction={userReaction}
+          reactions={commentReactions}
+          activityType={type}
+          postId={postId}
+          commentId={commentId}
+        />
+      </div>
+      <div className={style.activityCommentCount} onClick={() => onFocus()}>
+        <span>
+          Komentarze: <b>{subCommentsCount}</b>
+        </span>
+      </div>
+    </div>
+  );
+}
+// TODO!!
---
+++ frontend/src/components/features/Feed/components/Activity/Large/LargeActivity.tsx
@@ -0,0 +1,130 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import style from './LargeActivity.module.css';
+import { useAPI } from '../../../../../../hooks/useAPI';
+import { useContext, useEffect, useState } from 'react';
+import { ErrorHandler } from '../../../../../../lib/customError';
+import { customNotificationCtx } from '../../../../../../context/customNotificationContext';
+import UserActivityInfo from '../../UserActivityInfo/UserActivityInfo';
+import CommentCreator from '../../CommentCreator/CommentCreator';
+import ActivityReactions from '../../ActivityReactions/ActivityReactions';
+import { Activity } from '../Activity';
+import type { ReactionUnion } from '../../../../../../types/VisualUnions';
+
+
+
+interface ActivityElements {
+  postId: number;
+  id: number;
+  userId: number;
+  parentId: number;
+  content: string;
+  createdAt: string;
+  fullName: string;
+  reactions: Record<ReactionUnion, number>;
+  myReaction: ReactionUnion;
+  commentsCount:number
+  comments: ActivityElements[];
+}
+
+export function LargeActivity() {
+  const { commentid, postid } = useParams();
+  const { request } = useAPI();
+  const [activityData, setActivityData] = useState<ActivityElements | undefined>(undefined);
+  const [commentText, setCommentText] = useState<string>();
+  const ctx = useContext(customNotificationCtx);
+  const nav = useNavigate();
+  async function addComment() {
+    try {
+      const result = await request('/api/comments', 'POST', {
+        postId: +postid!,
+        parentId: commentid,
+        content: commentText,
+      });
+      if (!result || !result.success) {
+        throw new ErrorHandler(`Adding commment fault for ${commentid} `, 500);
+      }
+    } catch (error) {
+      if (error instanceof ErrorHandler) {
+        if (ctx) {
+          ctx.setNotify({ type: 'error', message: error.message });
+        } else {
+          console.error(error);
+        }
+      } else {
+        if (ctx) {
+          ctx.setNotify({ type: 'error', message: 'Unknown server error!' });
+        } else {
+          console.error(error);
+        }
+      }
+    }
+  }
+  useEffect(() => {
+    const query: string = commentid === undefined && postid ? `/api/posts/${postid}` : `/api/comments/${commentid}`;
+    request<ActivityElements>(query, 'GET')
+      .then((res) => {
+        if (!res || !res.success) {
+          throw new ErrorHandler('Failed to fetch comment', 400);
+        }
+        if (res.data) {
+          setActivityData(res.data);
+          console.log(res.data);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [commentid, request]);
+  return (
+    activityData && (
+      <div className={style.container}>
+        <div className={style.goBack}>
+          <span onClick={()=>nav(-1)} style={{cursor:'pointer'}}>X</span>
+        </div>
+        <div className={style.userInfoContainer}>
+          {activityData && (
+            <UserActivityInfo avatar={null} authorName={activityData.fullName} createdAt={activityData.createdAt} />
+          )}
+        </div>
+        <div className={style.contentContainer}>{activityData.content}</div>
+        <div className={style.reactionsContainer}>
+          { postid && (
+            <ActivityReactions
+              reactions={activityData.reactions}
+              postId={+postid}
+              activityType={commentid === undefined && postid ? 'POST' : 'COMMENT'}
+              myReaction={activityData.myReaction}
+            />
+          )}
+        </div>
+
+        <div className={style.commentCreatorContanier}>
+          <CommentCreator
+            onEditableTextAreaChange={(text: string) => setCommentText(text)}
+            text={commentText ?? ''}
+            onSubmit={addComment}
+          />
+        </div>
+        <div className={style.childComments}>
+           {activityData && postid &&
+            activityData.comments.map((v) => (
+              <Activity
+              key={v.id}
+                type="COMMENT"
+                avatar={null}
+                authorName={v.fullName}
+                createdAt={v.createdAt}
+                content={v.content}
+                commentId={v.id}
+                postId={+postid}
+                userReaction={v.myReaction}
+                commentReactions={v.reactions}
+                subCommentsCount={v.commentsCount}
+                onFocus={() => nav(`/post/${+postid}/comments/${v.id}`)}
+              />
+            ))}
+        </div>
+      </div>
+    )
+  );
+}
