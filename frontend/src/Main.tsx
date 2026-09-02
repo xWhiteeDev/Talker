6d2b9d2eb3fc7b7a6ff3ff9181db62b84e70df43
xWhiteeDev <WhiteeDev@proton.me>
2026-09-02 21:50:01 +0200

    feat: Add new routes

--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -1,56 +1,67 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Auth from "./components/features/Auth/components/AuthLayout.tsx";
-import { useState } from "react";
-import CustomNotification from "./components/generic/UI/Notification/CustomNotification.tsx";
-import Home from "./components/features/Home/Home.tsx";
-import Login from "./components/features/Auth/components/shared/Login/Login.tsx";
-import Register from "./components/features/Auth/components/shared/Register/Register.tsx";
-import { customNotificationCtx } from "./context/customNotificationContext.ts";
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Auth from './components/features/Auth/components/AuthLayout.tsx';
+import { useState } from 'react';
+import CustomNotification from './components/generic/UI/Notification/CustomNotification.tsx';
+import Home from './components/features/Home/Home.tsx';
+import Login from './components/features/Auth/components/shared/Login/Login.tsx';
+import Register from './components/features/Auth/components/shared/Register/Register.tsx';
+import { customNotificationCtx } from './context/customNotificationContext.ts';
 
-import ProtectedRoute from "./components/features/ProtectedRoute/ProtectedRoute.tsx";
-import AuthorizationProtectedRoute from "./components/features/ProtectedRoute/AuthorizationProtectedRoute.tsx";
-import "./Main.css";
-import type { NotificationHookProps } from "./types/NotificationHook";
-import { LargeActivity } from "./components/features/Feed/components/Activity/Large/LargeActivity.tsx";
+import ProtectedRoute from './components/features/ProtectedRoute/ProtectedRoute.tsx';
+import AuthorizationProtectedRoute from './components/features/ProtectedRoute/AuthorizationProtectedRoute.tsx';
+import './Main.css';
+import type { NotificationHookProps } from './types/NotificationHook';
+import { LargeActivity } from './components/features/Feed/components/Activity/Large/LargeActivity.tsx';
+import Profile from './components/features/Feed/components/profile/Profile.tsx';
 
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 const routes = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: (
           <ProtectedRoute>
             <Home />
           </ProtectedRoute>
         ),
-      },
-      {
-        path: "/post/:postid",
-        element: (
-          <ProtectedRoute>
-            <LargeActivity />
-          </ProtectedRoute>
-        ),
         children: [
           {
-            path: "comments/:commentid",
-            element: <LargeActivity />,
+            path: 'post/:postid',
+            element: (
+              <ProtectedRoute>
+                <LargeActivity />
+              </ProtectedRoute>
+            ),
+            children: [
+              {
+                path: 'comments/:commentid',
+                element: <LargeActivity />,
+              },
+            ],
+          },
+          {
+            path: 'profile/me',
+            element: <Profile />,
+          },
+            {
+            path: 'profile/:id',
+            element: <Profile />,
           },
         ],
       },
     ],
   },
   {
-    path: "/auth",
+    path: '/auth',
     element: <Auth />,
     children: [
       {
-        path: "login",
+        path: 'login',
         element: (
           <AuthorizationProtectedRoute>
             <Login />
@@ -58,7 +69,7 @@ const routes = createBrowserRouter([
         ),
       },
       {
-        path: "register",
+        path: 'register',
         element: (
           <AuthorizationProtectedRoute>
             <Register />
@@ -70,8 +81,7 @@ const routes = createBrowserRouter([
 ]);
 
 function Main() {
-  const [notification, setNotification] =
-    useState<NotificationHookProps | null>();
+  const [notification, setNotification] = useState<NotificationHookProps | null>();
 
   function setNotify(notiContext: NotificationHookProps) {
     if (notification) {
@@ -85,12 +95,7 @@ function Main() {
   return (
     <customNotificationCtx.Provider value={{ setNotify }}>
       {<RouterProvider router={routes} />}
-      {notification && (
-        <CustomNotification
-          type={notification.type}
-          message={notification.message}
-        />
-      )}
+      {notification && <CustomNotification type={notification.type} message={notification.message} />}
     </customNotificationCtx.Provider>
   );
 }
