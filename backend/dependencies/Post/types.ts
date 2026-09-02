0b6463dbfb72339f3141743766df6b262f84ebec
xWhiteeDev <WhiteeDev@proton.me>
2026-09-02 21:48:39 +0200

    Refactor: Typings change

--- backend/dependencies/Post/types.ts
+++ backend/dependencies/Post/types.ts
@@ -1,57 +1,51 @@
-import type {RowDataPacket} from "mysql2";
+import type { RowDataPacket } from 'mysql2';
 
 export interface IPostRepository {
-    findById(id: number, currentUserId?:number,withDetails?: boolean): Promise<PostRow | undefined>;
-    findByAuthor(authorId: number): Promise<PostRow[] | null>;
-    insert(dto: PostInsertDTO): Promise<boolean>;
-    update(id: number, dto: PostUpdateDTO): Promise<boolean>;
-    delete(id: number): Promise<boolean>;
-    findAll(userId: number): Promise<PostRow[]>;
-
+  findById(id: number, currentUserId?: number, withDetails?: boolean): Promise<PostRow | undefined>;
+  findByAuthor(authorId: number): Promise<PostRow[] | null>;
+  insert(dto: PostInsertDTO): Promise<boolean>;
+  update(id: number, dto: PostUpdateDTO): Promise<boolean>;
+  delete(id: number): Promise<boolean>;
+  findAll(userId: number): Promise<PostRow[]>;
 }
 export interface IPostService {
-    findById(userId: number, id: number,withDetails?: boolean): Promise<PostRow | null>;
-    findByAuthor(userId: number, authorId: number): Promise<PostRow[] | null>;
-    insertPost(dto: PostInsertDTO): Promise<boolean>;
-    updatePost(userId: number, id: number, dto: PostUpdateDTO): Promise<boolean>;
-    deletePost(userId: number, id: number): Promise<boolean>;
-    findLatestPosts(userId: number): Promise<PostRow[] | null>;
-}
-
-export interface IPostController {
-
+  findById(userId: number, id: number, withDetails?: boolean): Promise<PostRow | null>;
+  findByAuthor(userId: number, authorId: number): Promise<PostRow[] | null>;
+  insertPost(dto: PostInsertDTO): Promise<boolean>;
+  updatePost(userId: number, id: number, dto: PostUpdateDTO): Promise<boolean>;
+  deletePost(userId: number, id: number): Promise<boolean>;
+  findLatestPosts(userId: number): Promise<PostRow[] | null>;
 }
 
 export interface PostRow extends RowDataPacket {
-    id: number;
-    author: string;
-    authorId: number;
-    content: string;
-    visible_for: Visibility;
-    created_at: string;
-    photo?: string[];
-    video?: string[];
-    file?: string[];
-    gif?: string[];
-    tagged_users?: string[];
-    pinedPlace?: string;
-    firstName:string;
-    lastName:string
-
+  id: number;
+  createdAt: string;
+  authorId: number;
+  content: string;
+  visibleFor: Visibility;
+  fullName: string;
+  reactions: Record<string, number>;
+  myReaction: string;
+  commentsCount: number;
+  photo: string | null;
+  video: string | null;
+  file: string | null;
+  gif: string | null;
+  taggedUsers: string[] | null;
+  pinnedPlace: string | null;
 }
 
 export type PostInsertDTO = {
-    authorId: number;
-    content: string;
-    visible_for: Visibility;
-    photo?: string[];
-    video?: string[];
-    file?: string[];
-    gif?: string[];
-    tagged_users?: string[];
-    pinned_place?: string;
-}; 
-export interface PostUpdateDTO extends Omit<PostInsertDTO, 'author' | 'authorId'> { }
-
+  authorId: number;
+  content: string;
+  visible_for: Visibility;
+  photo?: string[];
+  video?: string[];
+  file?: string[];
+  gif?: string[];
+  tagged_users?: string[];
+  pinned_place?: string;
+};
+export interface PostUpdateDTO extends Omit<PostInsertDTO, 'author' | 'authorId'> {}
 
 type Visibility = 'Public' | 'friends' | 'private';
