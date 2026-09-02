9e40a8b5910e658624aab9987ab06d34585f86c6
xWhiteeDev <WhiteeDev@proton.me>
2026-09-02 21:51:45 +0200

    Feat: Add friends layer

---
+++ frontend/src/components/features/Home/components/FriendsLayer/FriendsLayer.tsx
@@ -0,0 +1,27 @@
+import style from './FriendsLayer.module.css';
+export default function FriendsLayer() {
+  return (
+    <div className={style.container}>
+      {/* <div className={style.friendsWindow}>
+        <div className={style.categoryHeader}>
+          <span>Active friends (14)</span>
+        </div>
+        <div className={style.friendsList}>
+          <ActiveFriend name="Jack" id={1} />
+          <ActiveFriend name="Jack" id={1} />
+          <ActiveFriend name="Jack" id={1} />
+          <ActiveFriend name="Jack" id={1} />
+          <ActiveFriend name="Jack" id={1} />
+          <ActiveFriend name="Jack" id={1} />
+          <ActiveFriend name="Jack" id={1} />
+          <ActiveFriend name="Jack" id={1} />
+          <ActiveFriend name="Jack" id={1} /> 
+          <ActiveFriend name="Jack" id={1} />
+          <ActiveFriend name="Jack" id={1} />
+          <ActiveFriend name="Jack" id={1} />
+          <ActiveFriend name="Jack" id={1} />
+        </div>
+      </div> */}
+    </div>
+  );
+}
