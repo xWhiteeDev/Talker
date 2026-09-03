9227e17f1eb71cf450a8b2cae97dec23c89ddb02
xWhiteeDev <WhiteeDev@proton.me>
2026-09-03 21:46:07 +0200

    Refactor: Refactor long code to shorter version of two types of request: Someone profile and my profile

--- backend/dependencies/Profile/profileService.ts
+++ backend/dependencies/Profile/profileService.ts
@@ -1,6 +1,7 @@
 import { ErrorHandler } from '../../handlers/errorHandler.js';
 import type { IAccountService } from '../Account/types.js';
 import type { PostService } from '../Post/postService.js';
+import type { PostRow } from '../Post/types.js';
 import type { IProfile, IProfileService } from './types.js';
 
 class ProfileService implements IProfileService {
@@ -9,52 +10,28 @@ class ProfileService implements IProfileService {
     private postService: PostService,
   ) {}
 
-  async getMe(userId: number): Promise<IProfile> {
-    try {
-      if (userId == undefined) {
-        throw new ErrorHandler('User ID not provided', 400);
-      }
-      if (typeof userId !== 'number') {
-        throw new ErrorHandler('User ID must be a number', 400);
-      }
-      const user = await this.accountService.findUserById(userId);
-      if (!user) {
-        throw new ErrorHandler('User not found', 404);
-      }
-      const content = await this.postService.findByAuthor(userId, userId);
-      return {
-        fullName: user.firstName + ' ' + user.lastName,
-        birthdayDate: user.birthdayDate,
-        joinDate: user.created_at,
-        description: 'Test description hardcoded by coder.',
-        content: content,
-      };
-    } catch (error) {
-      throw error;
-    }
-  }
   async get(userId: number, requestedId: number): Promise<IProfile> {
     try {
-      if (userId === requestedId) {
-        const res = await this.getMe(userId);
-        return res;
-      }
       if (userId == undefined) {
         throw new ErrorHandler('User ID not provided', 400);
       }
       if (typeof userId !== 'number') {
         throw new ErrorHandler('User ID must be a number', 400);
       }
-      const user = await this.accountService.findUserById(userId);
-      if (!user) {
-        throw new ErrorHandler('User not found', 404);
+      const accountProfileProperties =
+        userId !== requestedId
+          ? await this.accountService.findUserById(requestedId)
+          : await this.accountService.findUserById(userId);
+
+      if (!accountProfileProperties) {
+        throw new ErrorHandler('Account not found', 404);
       }
       const content = await this.postService.findByAuthor(userId, requestedId);
       return {
-        fullName: user.firstName + ' ' + user.lastName,
-        birthdayDate: user.birthdayDate,
-        joinDate: user.created_at,
-        description: 'Test description hardcoded by coder.',
+        fullName: accountProfileProperties.firstName + ' ' + accountProfileProperties.lastName,
+        birthdayDate: accountProfileProperties.birthdayDate,
+        joinDate: accountProfileProperties.created_at,
+        description: 'My hardcoded description',
         content: content,
       };
     } catch (error) {
