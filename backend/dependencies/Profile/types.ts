0f70658cfafe40682e9eef579a106b8e260f08d8
xWhiteeDev <WhiteeDev@proton.me>
2026-09-03 21:44:57 +0200

    Refactor: Delete unneeded methods with fetching my profile

--- backend/dependencies/Profile/types.ts
+++ backend/dependencies/Profile/types.ts
@@ -1,11 +1,9 @@
 import type { Request, Response, NextFunction } from 'express';
 interface IProfileService {
   get(userId: number, requestedId: number): Promise<IProfile>;
-  getMe(id: number): Promise<IProfile>;
 }
 interface IProfileController {
   getUser(req: Request, res: Response, next: NextFunction): Promise<boolean>;
-  getMe(req: Request, res: Response, next: NextFunction): Promise<boolean>;
 }
 interface IProfile {
   fullName: string;
