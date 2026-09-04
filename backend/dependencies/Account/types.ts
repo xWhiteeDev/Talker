f04355fe9aaac3c96d2c0697495e8423567e9f77
xWhiteeDev <WhiteeDev@proton.me>
2026-09-04 22:27:13 +0200

    feat: Add typings

--- backend/dependencies/Account/types.ts
+++ backend/dependencies/Account/types.ts
@@ -1,54 +1,56 @@
-import type {RowDataPacket} from "mysql2/promise";
+import type { RowDataPacket } from 'mysql2/promise';
 
 export interface IAccountRepository {
-    findWithCredentials(email: string): Promise<IAccountRow | null>;
-    findById(id: number): Promise<IAccountRow | null>;
+  findWithCredentials(email: string): Promise<IAccountRow | null>;
+  findById(id: number): Promise<IAccountRow | null>;
 
-    insert(data: IAccountInsertDTO): Promise<boolean>;
-    update(data: IAccountUpdateDTO): Promise<boolean>;
-    delete(email: string): Promise<boolean>;
-    isExist(email: string): Promise<boolean>;
-    findByCriteria(firstString?:string,lastString?:string ):Promise<IAccountRow[] | null>
+  insert(data: IAccountInsertDTO): Promise<boolean>;
+  update(data: IAccountUpdateDTO): Promise<boolean>;
+  delete(email: string): Promise<boolean>;
+  isExist(email: string): Promise<boolean>;
+  findByCriteria(firstString?: string, lastString?: string): Promise<IAccountRow[] | null>;
 }
 
 export interface IAccountService {
-    findUserWithCredentials(email: string): Promise<IAccountRow | null>;
-    findUserById(id: number): Promise<IAccountRow | null>;
-    insertUser(document: IAccountInsertDTO): Promise<boolean>;
-    updateUser(document: IAccountUpdateDTO): Promise<boolean>;
-    deleteUser(email: string): Promise<boolean>;
-    isUserExist(email: string): Promise<boolean>;
-    findUserByCriteria(firstString?:string,lastString?:string ):Promise<IAccountRow[] | null>
+  findUserWithCredentials(email: string): Promise<IAccountRow | null>;
+  findUserById(id: number): Promise<IAccountRow | null>;
+  insertUser(document: IAccountInsertDTO): Promise<boolean>;
+  updateUser(document: IAccountUpdateDTO): Promise<boolean>;
+  deleteUser(email: string): Promise<boolean>;
+  isUserExist(email: string): Promise<boolean>;
+  findUserByCriteria(firstString?: string, lastString?: string): Promise<IAccountRow[] | null>;
 }
 export interface IAccountRow extends RowDataPacket {
-    id: number;
-    firstName:string;
-    lastName:string;
-    birthdayDate:string;
-    created_at: string;
-    email: string;
-    password: string;
+  id: number;
+  firstName: string;
+  lastName: string;
+  birthdayDate: string;
+  created_at: string;
+  email: string;
+  password: string;
 }
 
 export interface IUser {
-    id: number;
-    access: string;
-    refresh: string;
+  username: string;
+  birthDate: string;
+  joinedAt: string;
+  email: string;
+  id: number;
+  access: string;
+  refresh: string;
 }
 
 export interface IAccountInsertDTO {
-    email: string;
-    password: string;
-    birthdayDate: string;
-    firstName: string;
-    lastName: string;
+  email: string;
+  password: string;
+  birthdayDate: string;
+  firstName: string;
+  lastName: string;
 }
 export interface IAccountUpdateDTO extends Partial<IAccountInsertDTO> {
-    email: string;
+  email: string;
 }
-
 
 export interface currentUser {
-    id: number;
+  id: number;
 }
-
