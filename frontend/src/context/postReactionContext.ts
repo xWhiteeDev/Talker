9b24f9571c7d2742d293270dc467ca01743c5cb3
xWhiteeDev <WhiteeDev@proton.me>
2026-09-04 22:27:57 +0200

    Refactor: Delete unneeded context

--- frontend/src/context/postReactionContext.ts
+++
@@ -1,4 +0,0 @@
-import {createContext} from "react";
-import type {PostReactionContext} from "../types/Context";
-
-export const postReactionCtx = createContext<PostReactionContext | undefined>(undefined);
