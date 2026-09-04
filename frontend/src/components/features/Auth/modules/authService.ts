e333ed8f4ce6d8b29344ed3810944204a2b0c7e5
xWhiteeDev <WhiteeDev@proton.me>
2026-09-04 22:22:40 +0200

    Refactor: Implement authContext environment to authService

--- frontend/src/components/features/Auth/modules/authService.ts
+++ frontend/src/components/features/Auth/modules/authService.ts
@@ -1,60 +1,68 @@
-import type {NavigateFunction} from "react-router-dom";
-import {emitServer} from "../../../../lib/API/emitServer";
-import {ErrorHandler} from "../../../../lib/customError";
-import {validate} from "../../../../services/validationService";
-import type {GlobalConfiguration, ValidationFunctions} from "../../../../types/Validator";
-import type {CustomNotificationContext} from "../../../../types/Context";
-import type {EmitResult} from "../../../../types/API";
-
+import { emitServer } from '../../../../lib/API/emitServer';
+import { ErrorHandler } from '../../../../lib/customError';
+import { validate } from '../../../../services/validationService';
+import type { GlobalConfiguration, ValidationFunctions } from '../../../../types/Validator';
+import type { CustomNotificationContext, IAuthContext } from '../../../../types/Context';
+import type { IUser } from '../../../../types/User';
+import type { NavigateFunction } from 'react-router-dom';
 
 interface AuthorizationInfo {
-    transmisionEndpoint: string;
-    successContent?:string;
-    navigationPoint: string;
-    validationConfiguration:GlobalConfiguration;
-    validationFunctions:ValidationFunctions
+  transmisionEndpoint: string;
+  successContent?: string;
+  validationConfiguration: GlobalConfiguration;
+  validationFunctions: ValidationFunctions;
 }
 
+export async function handleSubmitAuthForm(
+  event: React.SubmitEvent<HTMLFormElement>,
+  notificationContext: CustomNotificationContext,
+  authorizationContext: IAuthContext,
+  nav: NavigateFunction,
 
-export async function handleSubmitAuthForm(event: React.SubmitEvent<HTMLFormElement>, notificationContext: CustomNotificationContext, navigation: NavigateFunction, authorizationInfo: AuthorizationInfo) {
-    if (!event || !notificationContext || !navigation || Object.keys(authorizationInfo).length == 0) {
-        return false;
+  authorizationInfo: AuthorizationInfo,
+) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  const objectifiedFormData = Object.fromEntries(formData);
+  const validationResult = validate(
+    objectifiedFormData,
+    authorizationInfo.validationConfiguration,
+    authorizationInfo.validationFunctions,
+  );
+  if (!validationResult) {
+    notificationContext.setNotify({
+      type: 'error',
+      message: 'Validation failed!',
+    });
+    return false;
+  }
+  const transmisionUrl = `/api/auth/${authorizationInfo.transmisionEndpoint}`;
+  try {
+    const res = await emitServer<IUser>(transmisionUrl, 'POST', objectifiedFormData);
+    if (!res || (res && !res.success)) {
+      nav('/auth/login');
+      return
     }
-    event.preventDefault();
-
-    const formData = new FormData(event.currentTarget);
-    const objectifiedFormData = Object.fromEntries(formData);
-    const validationResult = validate(objectifiedFormData, authorizationInfo.validationConfiguration, authorizationInfo.validationFunctions);
-    if (!validationResult) {
-        notificationContext.setNotify({
-            type: 'error',
-            message: 'Validation failed!'
-        });
-        return false;
+    const loginResult = authorizationContext.login(res.data);
+    if (loginResult) {
+      nav('/');
+    } else {
+      nav('/auth/login');
     }
-    const transmisionUrl = `/api/auth/${authorizationInfo.transmisionEndpoint}`;
-    try {
-        await emitServer<EmitResult<unknown>>(transmisionUrl, 'POST', objectifiedFormData);
-        navigation(authorizationInfo.navigationPoint);
-        if (authorizationInfo.successContent) {
-            notificationContext.setNotify({
-                type: 'success',
-                message: authorizationInfo.successContent
-            });
-        }
-    } catch (error) {
-        if (error instanceof ErrorHandler) {
-            notificationContext.setNotify({
-                type: 'error',
-                message: error.message
-            });
-        } else {
-            notificationContext.setNotify({
-                type: 'error',
-                message: 'Unknown server error'
-            });
-        }
-        return false;
+    console.log(authorizationContext.user)
+  } catch (error) {
+    if (error instanceof ErrorHandler) {
+      notificationContext.setNotify({
+        type: 'error',
+        message: error.message,
+      });
+    } else {
+      notificationContext.setNotify({
+        type: 'error',
+        message: 'Unknown server error',
+      });
     }
-    return true;
+    return false;
+  }
+  return true;
 }
