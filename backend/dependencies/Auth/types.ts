f27efe05d2285af024823d5f58d912fef19d4e2f
xWhiteeDev <WhiteeDev@proton.me>
2026-09-04 22:25:45 +0200

    feat: Add IUserAuthorizationPassport typings

--- backend/dependencies/Auth/types.ts
+++ backend/dependencies/Auth/types.ts
@@ -1,18 +1,27 @@
-
-import type {NextFunction,Response,Request} from "express";
-import type {IAccountInsertDTO, IUser} from "../Account/types.js";
+import type { NextFunction, Response, Request } from 'express';
+import type { IAccountInsertDTO, IUser } from '../Account/types.js';
 
 export interface IAuthService {
-    signUp(document: IAccountInsertDTO): Promise<boolean | null>;
+  signUp(document: IAccountInsertDTO): Promise<boolean | null>;
 
-    signIn(document: ILogin): Promise<IUser | null>;
-    signNewToken(userId: number, tokenType: 'access' | 'refresh'): string;
+  signIn(document: ILogin): Promise<IUser | null>;
+  signNewToken(userId: number, tokenType: 'access' | 'refresh'): string;
+  isAuthorized(id: number): Promise<IUserAuthorizationPassport | null>;
 }
 export interface IAuthController {
-    createUser(req: Request, res: Response, next: NextFunction): Promise<boolean>;
-    signIn(req: Request, res: Response, next: NextFunction): Promise<boolean>;
+  createUser(req: Request, res: Response, next: NextFunction): Promise<boolean>;
+  signIn(req: Request, res: Response, next: NextFunction): Promise<boolean>;
+  isAuthorized(req: Request, res: Response, next: NextFunction):Promise<boolean>
 }
 export interface ILogin {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
+}
+
+export interface IUserAuthorizationPassport {
+  username: string;
+  email: string;
+  birthDate: string;
+  joinedAt: string;
+  id: number;
 }
