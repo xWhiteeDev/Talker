2eb82cd2019d55424988521c2815b087ae3a4289
xWhiteeDev <WhiteeDev@proton.me>
2026-09-04 22:24:42 +0200

    Refactor: Implement isAuthorized route with method

--- backend/routes/auth/authRoute.ts
+++ backend/routes/auth/authRoute.ts
@@ -1,74 +1,74 @@
-import express, {type Request, type Response} from "express";
-import {authController} from "../../loader/dependencyLoader.js";
-import {isDataValid, isRefreshTokenValid, isAccessTokenActive} from "../../middleware/middleware.js";
-import type {IRequirementOptions} from "../../middleware/types.js";
+import express, { type Request, type Response } from 'express';
+import { authController } from '../../loader/dependencyLoader.js';
+import { isDataValid, isRefreshTokenValid, isAccessTokenActive } from '../../middleware/middleware.js';
+import type { IRequirementOptions } from '../../middleware/types.js';
 
 export const authRouter = express.Router();
 
 const registerCfg: Record<string, IRequirementOptions> = {
-    email: {
-        minLength: 5,
-        maxLength: 35,
-        type: "string",
-        isRequired: true,
-        trimmed: true
-    },
-    password: {
-        minLength: 5,
-        maxLength: 200,
-        type: "string",
-        isRequired: true,
-        trimmed: true
-    },
-    firstName: {
-        minLength: 3,
-        maxLength: 15,
-        type: "string",
-        isRequired: true,
-        trimmed: true
-    },
-    lastName: {
-        minLength: 3,
-        maxLength: 15,
-        type: "string",
-        isRequired: true,
-        trimmed: true
-    },
-    birthdayDate: {
-        minLength: 3,
-        maxLength: 32,
-        type: "string",
-        isRequired: true,
-        trimmed: true
-    }
+  email: {
+    minLength: 5,
+    maxLength: 35,
+    type: 'string',
+    isRequired: true,
+    trimmed: true,
+  },
+  password: {
+    minLength: 5,
+    maxLength: 200,
+    type: 'string',
+    isRequired: true,
+    trimmed: true,
+  },
+  firstName: {
+    minLength: 3,
+    maxLength: 15,
+    type: 'string',
+    isRequired: true,
+    trimmed: true,
+  },
+  lastName: {
+    minLength: 3,
+    maxLength: 15,
+    type: 'string',
+    isRequired: true,
+    trimmed: true,
+  },
+  birthdayDate: {
+    minLength: 3,
+    maxLength: 32,
+    type: 'string',
+    isRequired: true,
+    trimmed: true,
+  },
 };
 
 const loginCfg: Record<string, IRequirementOptions> = {
-    email: {
-        minLength: 5,
-        maxLength: 35,
-        type: "string",
-        isRequired: true,
-        trimmed: true
-    },
-    password: {
-        minLength: 5,
-        maxLength: 200,
-        type: "string",
-        isRequired: true,
-        trimmed: true
-    },
+  email: {
+    minLength: 5,
+    maxLength: 35,
+    type: 'string',
+    isRequired: true,
+    trimmed: true,
+  },
+  password: {
+    minLength: 5,
+    maxLength: 200,
+    type: 'string',
+    isRequired: true,
+    trimmed: true,
+  },
 };
 authRouter.post('/register', isDataValid(registerCfg), async (req, res, next) => {
-    await authController.createUser(req, res, next);
+  await authController.createUser(req, res, next);
 });
 
 authRouter.post('/login', isDataValid(loginCfg), async (req, res, next) => {
-    await authController.signIn(req, res, next);
+  await authController.signIn(req, res, next);
 });
 authRouter.post('/refresh', isRefreshTokenValid(), async (req, res, next) => {
-    authController.createNewToken(req, res, next);
+  authController.createNewToken(req, res, next);
 });
-authRouter.get('/isAuth', isAccessTokenActive(), (req: Request, res, next) => {
-    res.status(200).json({success: true, data: {user: req.currentUser}});
+authRouter.get('/isAuth', isAccessTokenActive(), async (req: Request, res, next) => {
+  await authController.isAuthorized(req, res, next);
 });
