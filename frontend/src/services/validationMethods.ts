4e1f80bb1fedee03bea01f4a865442ca7dce214d
xWhiteeDev <WhiteeDev@proton.me>
2026-09-06 15:58:40 +0200

    Refactor: Change types convention and replace some vars to const

--- frontend/src/services/validationMethods.ts
+++ frontend/src/services/validationMethods.ts
@@ -1,12 +1,6 @@
-import type {
-  ConfigurationLength,
-  ValidationFunctions,
-} from "../types/Validator";
+import type { IConfigurationLength, IValidationFunctions } from '../types/services/IValidator';
 
-export function validateLength(
-  value: string,
-  rules: ConfigurationLength,
-): boolean {
+export function validateLength(value: string, rules: IConfigurationLength): boolean {
   return value.length <= rules.max && value.length >= rules.min;
 }
 
@@ -18,7 +12,7 @@ export function validateRegex(value: string, rules: RegExp): boolean {
   return rules.test(value);
 }
 
-export const validatorFunctions: ValidationFunctions = {
+export const validatorFunctions: IValidationFunctions = {
   trim: validateTrim,
   length: validateLength,
   regex: validateRegex,
@@ -39,11 +33,11 @@ export function getPersonAgeByDate(date: string): number | null {
   if (!isPassedDateValid) return null;
 
   const today = new Date();
-  let correctMonth = today.getMonth() + 1;
+  const correctMonth = today.getMonth() + 1;
 
-  const [birthYear, birthMonth, birthDay] = date.split("-").map(Number);
+  const [birthYear, birthMonth, birthDay] = date.split('-').map(Number);
 
-  let yearlyAge: number = today.getFullYear() - birthYear;
+  const yearlyAge: number = today.getFullYear() - birthYear;
 
   const calculatedBirth = birthMonth * 100 + birthDay;
   const calculatedToday = correctMonth * 100 + today.getDate();
@@ -55,7 +49,7 @@ export function getPersonAgeByDate(date: string): number | null {
 }
 
 export function isDateValid(date: string): boolean {
-  const dateSeparators = ["-", "/", "."];
+  const dateSeparators = ['-', '/', '.'];
   let separator: string | null = null;
   for (const dateSeparator of dateSeparators) {
     if (!date.includes(dateSeparator)) continue;
@@ -66,10 +60,8 @@ export function isDateValid(date: string): boolean {
   const [year, month, day]: number[] = date.split(separator).map(Number);
   if (isNaN(year) || isNaN(month) || isNaN(day)) return false;
   const today = new Date();
-  if (year >= today.getFullYear() + 1 || month > 12 || month < 1 || day < 1)
-    return false;
+  if (year >= today.getFullYear() + 1 || month > 12 || month < 1 || day < 1) return false;
   const testDate = new Date(year, month - 1, day);
-  if (testDate.getMonth() + 1 !== month || testDate.getDate() !== day)
-    return false;
+  if (testDate.getMonth() + 1 !== month || testDate.getDate() !== day) return false;
   return true;
 }
