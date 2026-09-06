6e2326b93298187d3629ca67e38ac1d0d8d0f27b
xWhiteeDev <WhiteeDev@proton.me>
2026-09-06 15:57:54 +0200

    Refactor: Refactor isUrlValid

--- frontend/src/services/guardService.ts
+++ frontend/src/services/guardService.ts
@@ -1,17 +1,16 @@
-
-
-
 export function isKeyValid<T>(key: string, allowed: (keyof T & string)[]): key is keyof T & string {
-    return allowed.includes(key as keyof T & string)
-};
-
-export function isUrlValid(endpoint: string): boolean {
-
-    try {
-        new URL(endpoint).origin
-        return true
-    } catch (error) {
-        return false
-    }
+  return allowed.includes(key as keyof T & string);
 }
 
+export function isUrlValid(endpoint: string): boolean {
+  try {
+    const url = new URL(endpoint).origin;
+    if (!url) {
+      throw new Error('URL is not valid');
+    }
+    return true;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
