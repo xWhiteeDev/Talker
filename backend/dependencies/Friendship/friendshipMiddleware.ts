dfc92ada62163aa01d616af37786cc16b2a15380
xWhiteeDev <WhiteeDev@proton.me>
2026-09-10 21:36:33 +0200

    Feat: Change validateOtherUser strategy

--- backend/dependencies/Friendship/friendshipMiddleware.ts
+++ backend/dependencies/Friendship/friendshipMiddleware.ts
@@ -2,13 +2,12 @@ import type {NextFunction, Request, Response} from "express";
 import {ErrorHandler} from '../../handlers/errorHandler.js';
 
 export function validateOtherUser() {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        const otherUserId: number = Number(req.query['otherUserId']);
-        if (isNaN(otherUserId)) {
-            next(new ErrorHandler('The otherUserId param is not a number!', 400));
+    return async (req: Request, _res: Response, next: NextFunction) => {
+        const otherId: number = Number(req.body.data.otherId);
+        if (isNaN(otherId)) {
+            next(new ErrorHandler('The otherId param is not a number!', 400));
             return ;
         }
-        req.otherUserId = otherUserId;
         next();
         return 
     };
