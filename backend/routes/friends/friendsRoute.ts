10cfd037d2e60ea9370e42603775edec000e7cc5
xWhiteeDev <WhiteeDev@proton.me>
2026-09-10 21:32:51 +0200

    Refactor: Change convention

--- backend/routes/friends/friendsRoute.ts
+++ backend/routes/friends/friendsRoute.ts
@@ -3,19 +3,19 @@ import {validateOtherUser} from '../../dependencies/Friendship/friendshipMiddlew
 import {friendsController} from '../../loader/dependencyLoader.js';
 import {isAccessTokenActive} from '../../middleware/middleware.js';
 
-export const router = express.Router();
+export const friendsRouter = express.Router();
 
 
-router.post('/invites', isAccessTokenActive(), validateOtherUser(), async (req, res, next) => {
+friendsRouter.post('/invites', isAccessTokenActive(), validateOtherUser(), async (req, res, next) => {
     await friendsController.setRelation(req, res, next);
 });
 
-router.patch('/invites', isAccessTokenActive(), validateOtherUser(), async (req, res, next) => {
+friendsRouter.patch('/invites', isAccessTokenActive(), validateOtherUser(), async (req, res, next) => {
     await friendsController.acceptRelation(req, res, next);
 });
-router.delete('/invites', isAccessTokenActive(), validateOtherUser(), async (req, res, next) => {
+friendsRouter.delete('/invites', isAccessTokenActive(), validateOtherUser(), async (req, res, next) => {
     await friendsController.removeRelation(req, res, next);
 });
-router.get('/invites', isAccessTokenActive(), validateOtherUser(), async (req, res, next) => {
+friendsRouter.get('/invites/:otherid', isAccessTokenActive(), validateOtherUser(), async (req, res, next) => {
     await friendsController.areInRelation(req, res, next);
 });
