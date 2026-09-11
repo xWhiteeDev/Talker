97cab2a341a692b60ccf084f11d76958c270868c
xWhiteeDev <WhiteeDev@proton.me>
2026-09-11 21:58:34 +0200

    Feat: Add guard of comment accesibility and comment post parent

--- backend/dependencies/CommentReaction/CommentReactionController.ts
+++ backend/dependencies/CommentReaction/CommentReactionController.ts
@@ -4,26 +4,33 @@ import type { CommentReactionService } from './CommentReactionService.js';
 import { ErrorHandler } from '../../handlers/errorHandler.js';
 import type { currentUser } from '../Account/types.js';
 import type { CommentReactionInsertDTO } from './types.js';
+import type { ICommentsService } from '../Comments/types.js';
 
 export class CommentReactionController {
   constructor(
     private commentReactionService: CommentReactionService,
     private postService: IPostService,
+    private commentsService: ICommentsService,
   ) {}
   async createReaction(req: Request, res: Response, next: NextFunction) {
-    const reactionBody = req.body.data;
-    const user: currentUser = req.currentUser;
-    const payload: CommentReactionInsertDTO = {
-      author_id: user.id,
-      type: reactionBody.type,
-      comment_id: reactionBody.commentId,
-    };
-    const canSeeThisPost = await this.postService.findById(user.id, reactionBody.postId);
-    if (!canSeeThisPost) {
-      next(new ErrorHandler('Access denied', 400));
-      return false;
-    }
     try {
+      const reactionBody = req.body.data;
+      const user: currentUser = req.currentUser;
+      const payload: CommentReactionInsertDTO = {
+        author_id: user.id,
+        type: reactionBody.type,
+        comment_id: reactionBody.commentId,
+      };
+      const requestedPost = await this.postService.findById(user.id, reactionBody.postId);
+      if (!requestedPost) {
+        next(new ErrorHandler('Access denied', 400));
+        return false;
+      }
+      const comment = await this.commentsService.findCommentByCommentId(user.id, reactionBody.commentId);
+      if (comment.postId !== requestedPost.id) {
+        next(new ErrorHandler('Comment is not part of that post!', 400));
+        return false;
+      }
       const result = await this.commentReactionService.insertCommentReaction(payload);
       if (!result) {
         next(new ErrorHandler('Failed to push reaction', 400));
