fd656c66f694d75cf3f6af075f8320a4a3f39185
xWhiteeDev <WhiteeDev@proton.me>
2026-09-11 21:54:22 +0200

    Feat: Add try catch statement

--- backend/dependencies/PostReaction/postReactionController.ts
+++ backend/dependencies/PostReaction/postReactionController.ts
@@ -1,8 +1,8 @@
-import { ErrorHandler } from "../../handlers/errorHandler.js";
-import type { currentUser } from "../Account/types.js";
-import type { IPostService } from "../Post/types.js";
-import type { IPostReactionsService, PostReactionInsertDTO } from "./types.js";
-import type { Request, Response, NextFunction } from "express";
+import { ErrorHandler } from '../../handlers/errorHandler.js';
+import type { currentUser } from '../Account/types.js';
+import type { IPostService } from '../Post/types.js';
+import type { IPostReactionsService, PostReactionInsertDTO } from './types.js';
+import type { Request, Response, NextFunction } from 'express';
 
 export class PostReactionController {
   constructor(
@@ -10,25 +10,22 @@ export class PostReactionController {
     private postService: IPostService,
   ) {}
   async createReaction(req: Request, res: Response, next: NextFunction) {
-    const reactionBody = req.body.data;
-    const user: currentUser = req.currentUser;
-    const payload: PostReactionInsertDTO = {
-      authorId: user.id,
-      type: reactionBody.type,
-      postId: reactionBody.postId,
-    };
-    const canSeeThisPost = await this.postService.findById(
-      user.id,
-      reactionBody.postId
-    );
-    if (!canSeeThisPost) {
-      next(new ErrorHandler("Access denied", 400));
-      return false;
-    }
     try {
+      const reactionBody = req.body.data;
+      const user: currentUser = req.currentUser;
+      const payload: PostReactionInsertDTO = {
+        authorId: user.id,
+        type: reactionBody.type,
+        postId: reactionBody.postId,
+      };
+      const canSeeThisPost = await this.postService.findById(user.id, reactionBody.postId);
+      if (!canSeeThisPost) {
+        next(new ErrorHandler('Access denied', 400));
+        return false;
+      }
       const result = await this.postReactionService.insertPostReaction(payload);
       if (!result) {
-        next(new ErrorHandler("Failed to push reaction", 400));
+        next(new ErrorHandler('Failed to push reaction', 400));
         return false;
       }
       res.status(201).json({ success: true, data: result });
@@ -42,12 +39,9 @@ export class PostReactionController {
     const reactionBody = req.body.data;
     const user: currentUser = req.currentUser;
     try {
-      const result = await this.postReactionService.deleteUserReactionInPost(
-        user.id,
-        reactionBody.postId,
-      );
+      const result = await this.postReactionService.deleteUserReactionInPost(user.id, reactionBody.postId);
       if (!result) {
-        next(new ErrorHandler("Failed to delete reaction", 400));
+        next(new ErrorHandler('Failed to delete reaction', 400));
         return false;
       }
       res.status(201).json({ success: true, data: result });
