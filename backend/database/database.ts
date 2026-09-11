22e70f412af9e79d4b522fae73e6df7db374a79e
xWhiteeDev <WhiteeDev@proton.me>
2026-09-11 21:59:54 +0200

    Feat: Move JWT env checking outside checking cfg statement

--- backend/database/database.ts
+++ backend/database/database.ts
@@ -1,30 +1,34 @@
-import mysql, { type Pool } from 'mysql2/promise'
-import dotEnv from 'dotenv'
-dotEnv.config()
+import mysql, { type Pool } from 'mysql2/promise';
+import dotEnv from 'dotenv';
+dotEnv.config();
 
 const cfg = {
-    user: process.env['TALKER_SERVER_DB_USER'] ?? 'root',
-    port: process.env['TALKER_SERVER_DB_PORT'] ?? 3306,
-    password: process.env['TALKER_SERVER_DB_PASSWORD'],
-    database: process.env['TALKER_SERVER_DB_DATABASE'],
-    waitForConnections: true,
-    connectionLimit: 15,
-    namedPlaceholders: true
-
-}
+  user: process.env['TALKER_SERVER_DB_USER'] ?? 'root',
+  port: process.env['TALKER_SERVER_DB_PORT'] ?? 3306,
+  password: process.env['TALKER_SERVER_DB_PASSWORD'],
+  database: process.env['TALKER_SERVER_DB_DATABASE'],
+  waitForConnections: true,
+  connectionLimit: 15,
+  namedPlaceholders: true,
+};
 
 export function createPool(): Pool {
-    if (!cfg.password || !cfg.database) {
-        if (!process.env['TALKER_SERVER_JWT_ACCESS_SECRET'] || !process.env['TALKER_SERVER_JWT_REFRESH_SECRET']) {
-            console.error('CRITICAL CONFIG POINT ISNT SET: TALKER_SERVER_JWT_ACCESS_SECRET OR  TALKER_SERVER_JWT_REFRESH_SECRET')
-            process.exit(1);
-        }
-        console.error('CRITICAL CONFIG POINT ISNT SET! PASSWORD OR DATABASE');
-        process.exit(1)
-    }
-    const pool = mysql.createPool({ user: cfg.user, port: +cfg.port, password: cfg.password, database: cfg.database, waitForConnections: cfg.waitForConnections, connectionLimit: cfg.connectionLimit, namedPlaceholders: cfg.namedPlaceholders });
-    return pool
+  if (!cfg.password || !cfg.database) {
+    console.error('CRITICAL CONFIG POINT ISNT SET! PASSWORD OR DATABASE');
+    process.exit(1);
+  }
+  if (!process.env['TALKER_SERVER_JWT_ACCESS_SECRET'] || !process.env['TALKER_SERVER_JWT_REFRESH_SECRET']) {
+    console.error('CRITICAL CONFIG POINT ISNT SET: TALKER_SERVER_JWT_ACCESS_SECRET OR  TALKER_SERVER_JWT_REFRESH_SECRET');
+    process.exit(1);
+  }
+  const pool = mysql.createPool({
+    user: cfg.user,
+    port: +cfg.port,
+    password: cfg.password,
+    database: cfg.database,
+    waitForConnections: cfg.waitForConnections,
+    connectionLimit: cfg.connectionLimit,
+    namedPlaceholders: cfg.namedPlaceholders,
+  });
+  return pool;
 }
-
-
-
