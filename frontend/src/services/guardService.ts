40876cd7d6d7c543da74ff75b12956673d91ca64
xWhiteeDev <WhiteeDev@proton.me>
2026-09-12 22:00:20 +0200

    Feat: Fix isUrlValid returns boolean

--- frontend/src/services/guardService.ts
+++ frontend/src/services/guardService.ts
@@ -6,11 +6,11 @@ export function isUrlValid(endpoint: string): boolean {
   try {
     const url = new URL(endpoint).origin;
     if (!url) {
-      throw new Error('URL is not valid');
+      return false
     }
     return true;
   } catch (error) {
     console.error(error);
-    throw error;
+    return false
   }
 }
