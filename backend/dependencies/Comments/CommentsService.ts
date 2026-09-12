b84b0fd06cbfb26de392237c43bf7baf7fed7750
xWhiteeDev <WhiteeDev@proton.me>
2026-09-12 20:49:06 +0200

    Feat: Change return value in methods when result is not true (I think is better solution than throwing Error..)

--- backend/dependencies/Comments/CommentsService.ts
+++ backend/dependencies/Comments/CommentsService.ts
@@ -1,4 +1,3 @@
-import { ErrorHandler } from '../../handlers/errorHandler.js';
 import type { CommentInsertDTO, CommentRow, FoundComment, ICommentsRepository, ICommentsService } from './types.js';
 
 export class CommentsService implements ICommentsService {
@@ -6,49 +5,49 @@ export class CommentsService implements ICommentsService {
   async findCommentsByPost(postId: number): Promise<CommentRow[]> {
     const result = await this.CommentsRepository.findByPost(postId);
     if (!result) {
-      throw new ErrorHandler('Comments not found', 400);
+      return [];
     }
     return result;
   }
   async findUserCommentsByPostId(postId: number): Promise<CommentRow[]> {
     const result = await this.CommentsRepository.findByPost(postId);
     if (!result) {
-      throw new ErrorHandler('Comments not found', 400);
+      return [];
     }
     return result;
   }
-  async findCommentByCommentId(userId: number, commentId: number): Promise<FoundComment> {
+  async findCommentByCommentId(userId: number, commentId: number): Promise<FoundComment | null> {
     const result = await this.CommentsRepository.findById(userId, commentId);
     if (!result) {
-      throw new ErrorHandler('Comments not found', 400);
+      return null;
     }
     return result;
   }
   async findCommentsByParentId(parentId: number): Promise<CommentRow[]> {
     const result = await this.CommentsRepository.findByParentId(parentId);
     if (!result) {
-      throw new ErrorHandler('Comments not found', 400);
+      return [];
     }
     return result;
   }
   async insertComment(dto: CommentInsertDTO): Promise<boolean> {
     const result = await this.CommentsRepository.insertDocument(dto);
     if (!result) {
-      throw new ErrorHandler('Operation failed', 400);
+      return false;
     }
     return result;
   }
   async updateCommentContent(commentId: number, newContent: string): Promise<boolean> {
     const result = await this.CommentsRepository.updateContent(commentId, newContent);
     if (!result) {
-      throw new ErrorHandler('Operation failed', 400);
+      return false;
     }
     return result;
   }
   async deleteComment(commentId: number): Promise<boolean> {
     const result = await this.CommentsRepository.deleteDocument(commentId);
     if (!result) {
-      throw new ErrorHandler('Operation failed', 400);
+      return false;
     }
     return result;
   }
