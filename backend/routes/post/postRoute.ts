7bb695c14fe35288cad9880c528e0aa7dbc22751
xWhiteeDev <WhiteeDev@proton.me>
2026-09-12 21:27:07 +0200

    Feat: Add config and validating coming data

--- backend/routes/post/postRoute.ts
+++ backend/routes/post/postRoute.ts
@@ -1,12 +1,30 @@
 import express from 'express';
-import { isAccessTokenActive } from '../../middleware/middleware.js';
+import { isAccessTokenActive, isDataValid } from '../../middleware/middleware.js';
 import { postController } from '../../loader/dependencyLoader.js';
+import type { IObjectRequirements } from '../../services/types.js';
 
 export const postRouter = express.Router();
 
+const config: IObjectRequirements = {
+  content: {
+    type: 'string',
+    requirements: {
+      trimmed: true,
+      minLength: 4,
+      maxLength: 400,
+    },
+  },
+  visible_for: {
+    type: 'string',
+    requirements: {
+      minLength: 5,
+      maxLength: 8,
+      expectedValues:['Public', 'Private', 'Friends']
+    },
+  },
+};
 
-
-postRouter.post('/', isAccessTokenActive(),async (req, res, next) => { //TODO: Validating post by middleware
+postRouter.post('/', isDataValid(config), isAccessTokenActive(), async (req, res, next) => {
   await postController.insertNewPost(req, res, next);
 });
 
