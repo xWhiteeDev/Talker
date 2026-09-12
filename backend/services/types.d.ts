829de3309e1c1365395204bd605248e3bca57b82
xWhiteeDev <WhiteeDev@proton.me>
2026-09-12 20:43:38 +0200

    feat: Add new validation method

---
+++ backend/services/types.d.ts
@@ -0,0 +1,46 @@
+export type Types = 'string' | 'number' | 'object' | 'array'
+
+export type RequirementsMap = {
+    string: IStringRequirements;
+    number: INumberRequirements;
+    array: IArrayRequirements;
+    object: IObjectRequirements
+}
+
+export type TValueMap = {
+    string: string;
+    number: number;
+    array: any[];
+    object: Record<string, unknown>
+}
+
+export type TRequirements = {
+    [K in Types]: {
+        type: K;
+        requirements: RequirementsMap[K]
+    }
+}[Types]
+
+export type TValidationHandler = {
+    [K in Types]: (toValidate: TValueMap[K], requirement: RequirementsMap[K]) => boolean
+}
+
+export interface INumberRequirements {
+    min: number;
+    max: number
+}
+export interface IStringRequirements {
+    minLength: number;
+    maxLength: number;
+    trimmed?: boolean;
+    regex?: RegExp
+}
+export interface IArrayRequirements {
+    minElements: number;
+    maxElements: number;
+}
+
+export interface IObjectRequirements {
+    [key: string]: TRequirements
+
+}
