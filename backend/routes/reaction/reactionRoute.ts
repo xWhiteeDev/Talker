7a7074d38c5fd646333ecaedf17df2ad5f91f2be
xWhiteeDev <WhiteeDev@proton.me>
2026-09-12 20:45:15 +0200

    feat: Add delete route for commentReactionRouter

--- backend/routes/reaction/reactionRoute.ts
+++ backend/routes/reaction/reactionRoute.ts
@@ -1,30 +1,24 @@
-import express from "express";
-import { isAccessTokenActive } from "../../middleware/middleware.js";
-import {
-  commentReactionController,
-  postReactionController,
-} from "../../loader/dependencyLoader.js";
+import express from 'express';
+import { isAccessTokenActive } from '../../middleware/middleware.js';
+import { commentReactionController, postReactionController } from '../../loader/dependencyLoader.js';
 
 export const postReactionRouter = express.Router();
 export const commentReactionRouter = express.Router();
 
-postReactionRouter.post("/", isAccessTokenActive(), async (req, res, next) => {
+postReactionRouter.post('/', isAccessTokenActive(), async (req, res, next) => {
   await postReactionController.createReaction(req, res, next);
 });
 
-postReactionRouter.delete(
-  "/",
-  isAccessTokenActive(),
-  async (req, res, next) => {
-    await postReactionController.deleteReaction(req, res, next);
-  },
-);
-//Comment routes
+postReactionRouter.delete('/', isAccessTokenActive(), async (req, res, next) => {
+  await postReactionController.deleteReaction(req, res, next);
+});
 
-commentReactionRouter.post(
-  "/",
-  isAccessTokenActive(),
-  async (req, res, next) => {
-    await commentReactionController.createReaction(req, res, next);
-  },
-);
+commentReactionRouter.post('/', isAccessTokenActive(), async (req, res, next) => {
+
+  await commentReactionController.createReaction(req, res, next);
+});
+
+commentReactionRouter.delete('/', isAccessTokenActive(), async (req, res, next) => {
+
+  await commentReactionController.deleteReaction(req, res, next);
+});
