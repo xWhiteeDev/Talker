ddaf5736bb22f1047a8f5d833b29ba515b12314a
xWhiteeDev <WhiteeDev@proton.me>
2026-09-12 22:01:43 +0200

    Feat: Change hardcoded path to path from .env

--- frontend/src/lib/API/refreshToken.ts
+++ frontend/src/lib/API/refreshToken.ts
@@ -1,10 +1,17 @@
+import { isUrlValid } from '../../services/guardService';
+
 export async function refreshToken() {
-    const res = await fetch('http://localhost:3000/api/auth/refresh', { method: 'POST', credentials: 'include' })
-    if (!res.ok) {
-        if (res.status === 401) {
-            return { success: false, requiresLogin: true }
-        }
-        return { success: false, requiresLogin: false }
+  const fullEndpoint = import.meta.env['VITE_SERVER_ENDPOINT'] + '/api/auth/refresh';
+  const isUrlCorrect: boolean = isUrlValid(fullEndpoint);
+  if (!isUrlCorrect) {
+    return false;
+  }
+  const res = await fetch(fullEndpoint, { method: 'POST', credentials: 'include' });
+  if (!res.ok) {
+    if (res.status === 401) {
+      return { success: false, requiresLogin: true };
     }
-    return { success: true, requiresLogin: false }
+    return { success: false, requiresLogin: false };
+  }
+  return { success: true, requiresLogin: false };
 }
