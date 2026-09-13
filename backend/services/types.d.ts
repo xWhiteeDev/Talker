bd9f0b4a260aeaa11ee605c40665ab89e89e41a4
xWhiteeDev <WhiteeDev@proton.me>
2026-09-13 18:51:48 +0200

    Feat: Add optionality to types and add canBeNull param

--- backend/services/types.d.ts
+++ backend/services/types.d.ts
@@ -1,47 +1,49 @@
-export type Types = 'string' | 'number' | 'object' | 'array'
+export type Types = 'string' | 'number' | 'object' | 'array';
 
 export type RequirementsMap = {
-    string: IStringRequirements;
-    number: INumberRequirements;
-    array: IArrayRequirements;
-    object: IObjectRequirements
-}
+  string: IStringRequirements;
+  number: INumberRequirements;
+  array: IArrayRequirements;
+  object: IObjectRequirements;
+};
 
 export type TValueMap = {
-    string: string;
-    number: number;
-    array: any[];
-    object: Record<string, unknown>
-}
+  string: string;
+  number: number;
+  array: any[];
+  object: Record<string, unknown>;
+};
 
 export type TRequirements = {
-    [K in Types]: {
-        type: K;
-        requirements: RequirementsMap[K]
-    }
-}[Types]
+  [K in Types]: {
+    type: K;
+    requirements: RequirementsMap[K];
+  };
+}[Types];
 
 export type TValidationHandler = {
-    [K in Types]: (toValidate: TValueMap[K], requirement: RequirementsMap[K]) => boolean
-}
+  [K in Types]: (toValidate: TValueMap[K], requirement: RequirementsMap[K]) => boolean;
+};
 
 export interface INumberRequirements {
-    min: number;
-    max: number
+  min?: number;
+  max?: number;
+  canBeNull?: boolean;
 }
 export interface IStringRequirements {
-    minLength: number;
-    maxLength: number;
-    trimmed?: boolean;
-    expectedValues?:string[]
-    regex?: RegExp
+  minLength?: number;
+  maxLength?: number;
+  trimmed?: boolean;
+  expectedValues?: string[];
+  regex?: RegExp;
+  canBeNull?: boolean;
 }
 export interface IArrayRequirements {
-    minElements: number;
-    maxElements: number;
+  minElements?: number;
+  maxElements?: number;
+  canBeNull?: boolean;
 }
 
 export interface IObjectRequirements {
-    [key: string]: TRequirements
-
+  [key: string]: TRequirements;
 }
