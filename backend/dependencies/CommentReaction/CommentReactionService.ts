fdf510f48525e66782e7069db1da0d4d28cfa3df
xWhiteeDev <WhiteeDev@proton.me>
2026-09-13 21:30:47 +0200

    Feat: Add reaction guard in insert and code format

--- backend/dependencies/CommentReaction/CommentReactionService.ts
+++ backend/dependencies/CommentReaction/CommentReactionService.ts
@@ -1,108 +1,68 @@
-import { ErrorHandler } from "../../handlers/errorHandler.js";
-import type { CommentReactionRepository } from "./CommentReactionRepository.js";
-import type {
-  CommentReactionInsertDTO,
-  CommentReactionRow,
-  ICommentReactionsService,
-} from "./types.js";
+import { ErrorHandler } from '../../handlers/errorHandler.js';
+import type { CommentReactionRepository } from './CommentReactionRepository.js';
+import type { CommentReactionInsertDTO, CommentReactionRow, ICommentReactionsService } from './types.js';
 
 export class CommentReactionService implements ICommentReactionsService {
   constructor(private CommentReactionRepository: CommentReactionRepository) {}
   async findCommentReactions(commentId: number): Promise<CommentReactionRow[]> {
-    const result =
-      await this.CommentReactionRepository.findByCommentId(commentId);
+    const result = await this.CommentReactionRepository.findByCommentId(commentId);
     if (result.length === 0) {
-      throw new ErrorHandler("Field not found", 400);
+      throw new ErrorHandler('Field not found', 400);
     }
     return result;
   }
-  async findCommentReactionsByType(
-    commentId: number,
-    type: string,
-  ): Promise<CommentReactionRow[]> {
-    const result = await this.CommentReactionRepository.findByType(
-      type,
-      commentId,
-    );
+  async findCommentReactionsByType(commentId: number, type: string): Promise<CommentReactionRow[]> {
+    const result = await this.CommentReactionRepository.findByType(type, commentId);
     if (result.length === 0) {
-      throw new ErrorHandler("Field not found", 400);
+      throw new ErrorHandler('Field not found', 400);
     }
     return result;
   }
   async findAllUserReactions(userId: number): Promise<CommentReactionRow[]> {
     const result = await this.CommentReactionRepository.findByAuthor(userId);
     if (result.length === 0) {
-      throw new ErrorHandler("Field not found", 400);
+      throw new ErrorHandler('Field not found', 400);
     }
     return result;
   }
-  async findUserReactionInComment(
-    userId: number,
-    commentId: number,
-  ): Promise<CommentReactionRow | undefined> {
-    const result = await this.CommentReactionRepository.findByAuthorInComment(
-      userId,
-      commentId,
-    );
+  async findUserReactionInComment(userId: number, commentId: number): Promise<CommentReactionRow | undefined> {
+    const result = await this.CommentReactionRepository.findByAuthorInComment(userId, commentId);
     if (!result) {
-      throw new ErrorHandler("Field not found", 400);
+      throw new ErrorHandler('Field not found', 400);
     }
     return result;
   }
   async insertCommentReaction(dto: CommentReactionInsertDTO): Promise<boolean> {
-    const existingReaction =
-      await this.CommentReactionRepository.findByAuthorInComment(
-        dto.author_id,
-        dto.comment_id,
-      );
+    const existingReaction = await this.CommentReactionRepository.findByAuthorInComment(dto.author_id, dto.comment_id);
     if (existingReaction) {
-      await this.CommentReactionRepository.delete(
-        dto.author_id,
-        dto.comment_id,
-      );
+      await this.CommentReactionRepository.delete(dto.author_id, dto.comment_id);
+      if (existingReaction.type === dto.type) return true;
     }
     const result = await this.CommentReactionRepository.insert(dto);
     if (!result) {
-      throw new ErrorHandler("Operation failed", 400);
+      throw new ErrorHandler('Operation failed', 400);
     }
     return result;
   }
-  async updateReactionType(
-    reactionId: number,
-    newType: string,
-  ): Promise<boolean> {
-    const existingResult =
-      await this.CommentReactionRepository.findById(reactionId);
+  async updateReactionType(reactionId: number, newType: string): Promise<boolean> {
+    const existingResult = await this.CommentReactionRepository.findById(reactionId);
     if (!existingResult) {
-      throw new ErrorHandler("Field not found", 400);
+      throw new ErrorHandler('Field not found', 400);
     }
-    const result = await this.CommentReactionRepository.updateType(
-      reactionId,
-      newType,
-    );
+    const result = await this.CommentReactionRepository.updateType(reactionId, newType);
     if (!result) {
-      throw new ErrorHandler("Operation failed", 400);
+      throw new ErrorHandler('Operation failed', 400);
     }
     return result;
   }
-  async deleteUserReactionInComment(
-    userId: number,
-    commentId: number,
-  ): Promise<boolean> {
-    const existingResult =
-      await this.CommentReactionRepository.findByAuthorInComment(
-        userId,
-        commentId,
-      );
+  async deleteUserReactionInComment(userId: number, commentId: number): Promise<boolean> {
+    const existingResult = await this.CommentReactionRepository.findByAuthorInComment(userId, commentId);
     if (!existingResult) {
-      throw new ErrorHandler("Field not found", 400);
+      throw new ErrorHandler('Field not found', 400);
     }
-    const result = await this.CommentReactionRepository.delete(
-      userId,
-      commentId,
-    );
+    const result = await this.CommentReactionRepository.delete(userId, commentId);
     if (!result) {
-      throw new ErrorHandler("Operation failed", 400);
+      throw new ErrorHandler('Operation failed', 400);
     }
     return result;
   }
