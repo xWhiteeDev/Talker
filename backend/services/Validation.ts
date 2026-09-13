6d4642dd0314616f22573b49c88ff3972e22cd80
xWhiteeDev <WhiteeDev@proton.me>
2026-09-13 18:46:09 +0200

    Feat: Add optionality too all options in Validation (No every option is needed)

--- backend/services/Validation.ts
+++ backend/services/Validation.ts
@@ -16,49 +16,56 @@ export class ReqValid {
   };
   static validateObject(toValidate: Record<string, unknown>, requirements: IObjectRequirements) {
     let passed: boolean = false;
-    for (const requirementKey in requirements) {
-      const requirement = requirements[requirementKey];
-      if (!requirement) {
-        console.error('Requirement key missing!');
-        return false;
-      }
-      if (requirement.type === 'number') {
-        passed = ReqValid.validationMethods.number(toValidate[requirementKey] as number, requirement.requirements);
-      }
-      if (requirement.type === 'string') {
-        passed = ReqValid.validationMethods.string(toValidate[requirementKey] as string, requirement.requirements);
-      }
-      if (requirement.type === 'array') {
-        passed = ReqValid.validationMethods.array(toValidate[requirementKey] as any[], requirement.requirements);
-      }
-      if (requirement.type === 'object') {
-        passed = ReqValid.validationMethods.object(
-          toValidate[requirementKey] as Record<string, unknown>,
-          requirement.requirements,
-        );
-      }
-      if (!passed) {
-        return false;
+    if (Object.keys(requirements).length !== 0) {
+      for (const requirementKey in requirements) {
+        const requirement = requirements[requirementKey];
+        if (!requirement) {
+          console.error('Requirement key missing!');
+          return false;
+        }
+        if (requirement.type === 'number') {
+          passed = ReqValid.validationMethods.number(toValidate[requirementKey] as number, requirement.requirements);
+        }
+        if (requirement.type === 'string') {
+          passed = ReqValid.validationMethods.string(toValidate[requirementKey] as string, requirement.requirements);
+        }
+        if (requirement.type === 'array') {
+          passed = ReqValid.validationMethods.array(toValidate[requirementKey] as any[], requirement.requirements);
+        }
+        if (requirement.type === 'object') {
+          passed = ReqValid.validationMethods.object(
+            toValidate[requirementKey] as Record<string, unknown>,
+            requirement.requirements,
+          );
+        }
+        if (!passed) {
+          return false;
+        }
       }
     }
+
     return true;
   }
   static validateNumber(toValidate: number, requirements: INumberRequirements): boolean {
     if (isNaN(toValidate)) {
       return false;
     }
-    if (toValidate < requirements.min) {
+    if (!requirements.canBeNull && toValidate == null) {
+      console.error(`Validated ${toValidate} is null but cannot be.`);
+      return false;
+    }
+    if (requirements.min && toValidate < requirements.min) {
       console.error(`Validated ${toValidate} is too low`);
       return false;
     }
-    if (toValidate > requirements.max) {
+    if (requirements.max && toValidate > requirements.max) {
       console.error(`Validated ${toValidate} is too high`);
       return false;
     }
     return true;
   }
   static validateString(toValidate: string, requirements: IStringRequirements): boolean {
-    if (typeof toValidate !== 'string') {
+    if (typeof toValidate !== 'string' || (!requirements.canBeNull && toValidate == null)) {
       console.error('Value is not a string!');
       return false;
     }
@@ -68,23 +75,28 @@ export class ReqValid {
     if (requirements.expectedValues && !requirements.expectedValues.includes(toValidate)) {
       return false;
     }
-    if (requirements.regex) {
-      return requirements.regex.test(toValidate);
-    }
-    if (toValidate.length < requirements.minLength) {
+
+    if (requirements.minLength && toValidate.length < requirements.minLength) {
       console.error(`Validated ${toValidate} is too short`);
       return false;
     }
-    if (toValidate.length > requirements.maxLength) {
+    if (requirements.maxLength && toValidate.length > requirements.maxLength) {
       console.error(`Validated ${toValidate} is too long`);
       return false;
+    }
+    if (requirements.regex) {
+      return requirements.regex.test(toValidate);
     }
     return true;
   }
   static validateArray(toValidate: any[], requirements: IArrayRequirements) {
     if (!Array.isArray(toValidate)) return false;
-    if (toValidate.length < requirements.minElements) return false;
-    if (toValidate.length > requirements.maxElements) return false;
+    if (!requirements.canBeNull && toValidate == null) {
+      console.error(`Validated ${toValidate} is null but cannot be.`);
+      return false;
+    }
+    if (requirements.minElements && toValidate.length < requirements.minElements) return false;
+    if (requirements.maxElements && toValidate.length > requirements.maxElements) return false;
     return true;
   }
 }
