0fc0d4a66c474ddf132a5de5e35069ba75503079
xWhiteeDev <WhiteeDev@proton.me>
2026-09-15 12:33:49 +0200

    Feat: Fix loading style

--- frontend/src/components/generic/UI/Loading/Loading.tsx
+++ frontend/src/components/generic/UI/Loading/Loading.tsx
@@ -1,15 +1,13 @@
-import Logo from "../Logo/Logo";
-import CustomText from "../Text/Text";
-import style from './Loading.module.css'
+import Logo from '../Logo/Logo';
+import CustomText from '../Text/Text';
+import style from './Loading.module.css';
 export function Loading() {
   return (
     <div className={style.loadingScene}>
-      <Logo additionalStyle={{ width: "46%", gridColumn: "2/3" }} />
-      <CustomText
-        text="Loading..."
-        bottomText="Be patient"
-        additionalStyle={{ fontSize: "1.2rem", textAlign: "center" }}
-      />
+      <div className={style.items}>
+        <Logo additionalStyle={{ width: '46%', gridColumn: '2/3' }} />
+        <CustomText text="Loading..." bottomText="Be patient" additionalStyle={{ fontSize: '1.2rem', textAlign: 'center' }} />
+      </div>
     </div>
   );
 }
