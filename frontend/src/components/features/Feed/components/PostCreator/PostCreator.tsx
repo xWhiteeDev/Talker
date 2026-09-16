91e82b9c6d98a63ba76c1616c15810276bc0d10f
xWhiteeDev <WhiteeDev@proton.me>
2026-09-16 12:33:41 +0200

    Feat: Style update

--- frontend/src/components/features/Feed/components/PostCreator/PostCreator.tsx
+++ frontend/src/components/features/Feed/components/PostCreator/PostCreator.tsx
@@ -1,13 +1,7 @@
-
-import Button from "../../../../generic/UI/Button/Button";
-import EditableTextArea from "../../../../generic/UI/EditableTextArea/EditableTextArea";
-import OptionList from "../../../../generic/UI/OptionList/OptionList";
-import {AddFromGalleryIcon} from "../../../Svgicons/PostCreators/AddFromGalleryIcon";
-import {AddGifIcon} from "../../../Svgicons/PostCreators/AddGifIcon";
-import {AddPhotoIcon} from "../../../Svgicons/PostCreators/AddPhotoIcon";
-import {PinFriendsIcon} from "../../../Svgicons/PostCreators/PinFriendsIcon";
-import {PinPlaceIcon} from "../../../Svgicons/PostCreators/PinPlaceIcon";
-import style from "./PostCreator.module.css";
+import Button from '../../../../generic/UI/Button/Button';
+import EditableTextArea from '../../../../generic/UI/EditableTextArea/EditableTextArea';
+import OptionList from '../../../../generic/UI/OptionList/OptionList';
+import style from './PostCreator.module.css';
 
 interface PostCreatorProps {
   onWrite?(text: string): void;
@@ -16,72 +10,60 @@ interface PostCreatorProps {
   text: string | undefined;
 }
 
-export function PostCreator({
-  onWrite,
-  onConfirm,
-  onOptionChange,
-  text,
-}: PostCreatorProps) {
+export function PostCreator({ onWrite, onConfirm, onOptionChange, text }: PostCreatorProps) {
   return (
     <div className={style.addPost}>
       {text && text.length > 0 ? (
         <OptionList
           placeholderText="Select visibility"
-          itemsList={["Public", "Friends", "Private"]}
+          itemsList={['Public', 'Friends', 'Private']}
           additionalStyle={{
-            width: "7vw",
-            margin: "2%",
-            borderRadius: "10px",
+            width: '7vw',
+            margin: '2%',
+            borderRadius: '5px',
           }}
           onOptionChange={(v) => onOptionChange && onOptionChange(v)}
         />
       ) : null}
       <EditableTextArea
         placeholderColor="black"
-        placeholder={"+ Share something with world"}
+        placeholder={'+ Share something with world'}
         placeholderFontWeight="600"
         max={299}
         onInput={(text) => onWrite && onWrite(text)}
         additionalStyle={{
-          padding: "2%",
-          width: "29.5vw",
-          maxWidth: "100%",
-          minHeight: "1vh",
-          color: "black",
+          padding: '2%',
+          width: '29.5vw',
+          maxWidth: '100%',
+          minHeight: '1vh',
+          color: 'black',
         }}
         securedText={text}
       />
       {text && text.length > 0 ? (
         <div className={style.postTools}>
-          <div className={style.attachments}>
-            <PinFriendsIcon />
-            <PinPlaceIcon />
-
-            <AddPhotoIcon />
-            <AddFromGalleryIcon />
-            <AddGifIcon />
-          </div>
-          <span
-            className={style.textLengthCounter}
-            style={{ color: text.length > 299 ? "red" : "black" }}
-          >
+          <span className={style.textLengthCounter} style={{ color: text.length > 299 ? 'red' : 'black' }}>
             {text.length} / {300}
           </span>
           <Button
             text="Add post"
             additionalStyle={{
-              width: "auto",
-              border: "none",
-              justifySelf: "center",
-              alignSelf: "center",
-              backgroundColor: " rgba(65, 199, 252, 0.432)",
-              boxShadow: "2px 1px 2px 1.2px #71a0ccb0",
-              color: "rgb(49, 49, 49)",
-              borderRadius: "10px",
+              width: 'auto',
+              border: 'none',
+              justifySelf: 'center',
+              alignSelf: 'center',
+              padding: '0.5vh',
+              backgroundColor: 'rgba(65, 199, 252, 0.432)',
+              boxShadow: '2px 1px 2px 1.2px #71a0ccb0',
+              color: 'rgb(49, 49, 49)',
+              borderRadius: '5px',
+              fontWeight: '600',
+              height: '25%',
             }}
             onClick={async () => {
               if (onConfirm) onConfirm();
             }}
+            isDisabled={text.length < 4}
           />
         </div>
       ) : null}
