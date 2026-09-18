7be95a499d607a8b361b284f541dbe3ee9e09082
xWhiteeDev <WhiteeDev@proton.me>
2026-09-18 13:41:56 +0200

    test: complete unit tests for friendshipService

---
+++ backend/__tests__/deps/friendships/friendshipService.test.ts
@@ -0,0 +1,151 @@
+import { describe, expect, it, vi } from 'vitest';
+import { friendshipService } from '../../../dependencies/Friendship/friendshipService.js';
+import type { FriendsRelation, FriendsRelationInsertDTO, FriendsRelationRow } from '../../../dependencies/Friendship/types.js';
+
+describe('findRelationBetween', () => {
+  it('should return null when relation not found', async () => {
+    const fakeRepo = {
+      findRelationBetween: vi.fn().mockResolvedValue(null),
+    };
+    const service = new friendshipService(fakeRepo as any);
+    await expect(service.findRelationBetween(1, 2, 'accepted')).resolves.toBeNull();
+  });
+  it('should return relation', async () => {
+    const payload: FriendsRelation = {
+      id: 3,
+      userId: 1,
+      friendId: 2,
+      status: 'accepted',
+      created_at: 0,
+    } as FriendsRelation;
+
+    const fakeRepo = {
+      findRelationBetween: vi.fn().mockResolvedValue(payload),
+    };
+
+    const service = new friendshipService(fakeRepo as any);
+    await expect(service.findRelationBetween(1, 2, 'accepted')).resolves.toEqual(payload);
+  });
+  it('should pass arguments recevived correctly by function', async () => {
+    const fakeRepo = {
+      findRelationBetween: vi.fn(),
+    };
+    const service = new friendshipService(fakeRepo as any);
+    await service.findRelationBetween(1, 2, 'pending');
+    expect(fakeRepo.findRelationBetween).toHaveBeenCalledWith(1, 2, 'pending');
+  });
+  it('should return null when passed status argument is not allowed', async () => {
+    const fakeRepo = {};
+    const service = new friendshipService(fakeRepo as any);
+    await expect(service.findRelationBetween(1,2,'anyOtherStatus')).resolves.toBe(null);
+  });
+});
+
+describe('insertRelation', () => {
+  it('should throw an error when relation with that user exists', async () => {
+    const payload: FriendsRelationInsertDTO = {
+      userId: 1,
+      friendId: 2,
+      status: 'accepted',
+    };
+    const fakeRepo = {
+      findRelationBetween: vi.fn().mockResolvedValue(true),
+    };
+    const service = new friendshipService(fakeRepo as any);
+    await expect(service.insertRelation(payload)).rejects.toThrow('Relation found with exact user');
+  });
+
+  it('should return true when record inserted', async () => {
+    const payload: FriendsRelationInsertDTO = {
+      userId: 1,
+      friendId: 2,
+      status: 'accepted',
+    };
+    const fakeRepo = {
+      findRelationBetween: vi.fn().mockResolvedValue(false),
+      insert: vi.fn().mockResolvedValue(true),
+    };
+    const service = new friendshipService(fakeRepo as any);
+    await expect(service.insertRelation(payload)).resolves.toBe(true);
+  });
+  it('should throw an error when trying to add yourself', async () => {
+    const payload: FriendsRelationInsertDTO = {
+      userId: 1,
+      friendId: 1,
+      status: 'accepted',
+    };
+    const fakeRepo = {};
+    const service = new friendshipService(fakeRepo as any);
+    await expect(service.insertRelation(payload)).rejects.toThrow('Cannot add yourself as friend');
+  });
+});
+
+describe('updateRelation', () => {
+  it('should throw an error when try to update relation which is not found', async () => {
+    const fakeRepo = {
+      findRelationBetween: vi.fn().mockResolvedValue(null),
+    };
+    const service = new friendshipService(fakeRepo as any);
+    await expect(service.updateRelation(1, 2, { status: 'accepted' })).rejects.toThrow('Relation not found with exact user');
+  });
+  it('should return true when update succeed', async () => {
+    const fakeRepo = {
+      findRelationBetween: vi.fn().mockResolvedValue({ userId: 2, friendId: 1 } as FriendsRelationRow),
+      update: vi.fn().mockResolvedValue(true),
+    };
+    const service = new friendshipService(fakeRepo as any);
+    await expect(service.updateRelation(1, 2, { status: 'accepted' })).resolves.toBe(true);
+  });
+  it('should throw an error when initiator trying to accept sent request', async () => {
+    const fakeRepo = {
+      findRelationBetween: vi.fn().mockResolvedValue({ userId: 1, friendId: 2 } as FriendsRelationRow),
+    };
+    const service = new friendshipService(fakeRepo as any);
+    await expect(service.updateRelation(1, 2, { status: 'accepted' })).rejects.toThrow('Cannot accept yourself');
+  });
+});
+
+describe('acceptRelation', () => {
+  it('should throw an error when try to accept relation which is not found', async () => {
+    const fakeRepo = {
+      findRelationBetween: vi.fn().mockResolvedValue(null),
+    };
+    const service = new friendshipService(fakeRepo as any);
+    await expect(service.acceptRelation(1, 2)).rejects.toThrow('Relation not found with exact user');
+  });
+
+  it('should return true when relation accepted', async () => {
+    const fakeRepo = {
+      findRelationBetween: vi.fn().mockResolvedValue({ userId: 2, friendId: 1 } as FriendsRelationRow),
+      update: vi.fn().mockResolvedValue(true),
+    };
+    const service = new friendshipService(fakeRepo as any);
+    await expect(service.acceptRelation(1, 2)).resolves.toBe(true);
+  });
+
+  it('should throw an error when initiator trying to accept sent request', async () => {
+    const fakeRepo = {
+      findRelationBetween: vi.fn().mockResolvedValue({ userId: 1, friendId: 2 } as FriendsRelationRow),
+    };
+    const service = new friendshipService(fakeRepo as any);
+    await expect(service.acceptRelation(1, 2)).rejects.toThrow('You cannot accept relation offer sent by you');
+  });
+});
+
+describe('removeRelation', () => {
+  it('should throw an error when such relation not found', async () => {
+    const fakeRepo = {
+      findRelationBetween: vi.fn().mockResolvedValue(null),
+    };
+    const service = new friendshipService(fakeRepo as any);
+    await expect(service.removeRelation(1, 2)).rejects.toThrow('Relation not found with exact user');
+  });
+  it('should return true when relation removed', async () => {
+    const fakeRepo = {
+      findRelationBetween: vi.fn().mockResolvedValue({ userId: 1, friendId: 2 }),
+      delete: vi.fn().mockResolvedValue(true),
+    };
+    const service = new friendshipService(fakeRepo as any);
+    await expect(service.removeRelation(1, 2)).resolves.toBe(true);
+  });
+});
