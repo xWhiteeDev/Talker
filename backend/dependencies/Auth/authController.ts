34e2827bee598c6848317529b095bcc875a66286
WhiteeDev <WhiteeDev@proton.me>
2026-09-23 21:18:43 +0200

    Feat: Add return false in catch

--- backend/dependencies/Auth/authController.ts
+++ backend/dependencies/Auth/authController.ts
@@ -1,8 +1,8 @@
-import type { NextFunction, Request, Response } from 'express';
-import { configDotenv } from 'dotenv';
-import { ErrorHandler } from '../../handlers/errorHandler.js';
-import type { IAuthController, IAuthService, ILogin, IUserAuthorizationPassport, IUserRegisterPayload } from './types.js';
-import type { currentUser } from '../Account/types.js';
+import type {NextFunction, Request, Response} from 'express';
+import {configDotenv} from 'dotenv';
+import {ErrorHandler} from '../../handlers/errorHandler.js';
+import type {IAuthController, IAuthService, ILogin, IUserAuthorizationPassport, IUserRegisterPayload} from './types.js';
+import type {currentUser} from '../Account/types.js';
 
 configDotenv();
 export class AuthController implements IAuthController {
@@ -25,12 +25,12 @@ export class AuthController implements IAuthController {
       if (!result) {
         throw new ErrorHandler('Failed to create account', 400, false);
       }
-      res.status(200).json({ success: true, data: result });
+      res.status(200).json({success: true, data: result});
       return true;
     } catch (error) {
       next(error);
+      return false;
     }
-    return true;
   }
   async signIn(req: Request, res: Response, next: NextFunction): Promise<boolean> {
     try {
@@ -42,6 +42,7 @@ export class AuthController implements IAuthController {
         email: data.email,
         password: data.password,
       });
+
       if (!signResult) {
         throw new ErrorHandler('Failed to sign in', 400, false);
       }
@@ -68,19 +69,20 @@ export class AuthController implements IAuthController {
         id: signResult.id,
       };
       req['currentUser'] = currentUserPayload;
-      res.status(200).json({ success: true, data: currentUserPayload });
+      res.status(200).json({success: true, data: currentUserPayload});
+
       return true;
     } catch (error) {
       next(error);
+      return false;
     }
-    return true;
   }
   createNewToken(req: Request, res: Response, next: NextFunction) {
     if (!req.currentUser) {
       next(new ErrorHandler('Unauthorized', 401, true));
       return false;
     }
-    const { id } = req.currentUser;
+    const {id} = req.currentUser;
     const signedToken = this.authService.signNewToken(id, 'access');
     res.cookie('accessToken', signedToken, {
       httpOnly: true,
@@ -88,7 +90,7 @@ export class AuthController implements IAuthController {
       secure: true,
       maxAge: 5 * 60 * 1000,
     });
-    res.status(200).json({ success: true, data: { id } });
+    res.status(200).json({success: true, data: {id}});
     return true;
   }
   async isAuthorized(req: Request, res: Response, next: NextFunction): Promise<boolean> {
@@ -98,7 +100,7 @@ export class AuthController implements IAuthController {
       }
       const user = await this.authService.isAuthorized(+req.currentUser.id);
       if (!user) {
-       throw new ErrorHandler('User not exist', 401);
+        throw new ErrorHandler('User is not authorized', 401);
       }
       const currentUserPayload = {
         username: user.username,
@@ -108,7 +110,7 @@ export class AuthController implements IAuthController {
         id: user.id,
       };
       req['currentUser'] = currentUserPayload;
-      res.status(200).json({ success: true, data: currentUserPayload });
+      res.status(200).json({success: true, data: currentUserPayload});
       return true;
     } catch (error) {
       next(error);
