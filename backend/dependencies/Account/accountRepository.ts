fff6973e3c4fd244658d3a2bfee3dcc3869af03f
WhiteeDev <WhiteeDev@proton.me>
2026-09-24 21:14:54 +0200

    Feat:Fix broken logic for criteria search in account repository

--- backend/dependencies/Account/accountRepository.ts
+++ backend/dependencies/Account/accountRepository.ts
@@ -1,36 +1,44 @@
-import { type ResultSetHeader, type ExecuteValues, type Pool, type RowDataPacket } from 'mysql2/promise';
-import type { IAccountRepository, IAccountRow, IAccountInsertDTO, IAccountUpdateDTO } from './types.js';
+import {type ResultSetHeader, type ExecuteValues, type Pool, type RowDataPacket} from 'mysql2/promise';
+import type {IAccountRepository, IAccountRow, IAccountInsertDTO, IAccountUpdateDTO} from './types.js';
 
 export class AccountRepository implements IAccountRepository {
   constructor(private pool: Pool) {
   }
   async findWithCredentials(email: string): Promise<IAccountRow | null> {
-    const [[result]] = await this.pool.query<IAccountRow[]>('SELECT * FROM accounts WHERE email=:email LIMIT 1', { email });
-    if (!result) return null;
-    return result;
+    const [result] = await this.pool.query<IAccountRow[]>('SELECT * FROM accounts WHERE email=:email LIMIT 1', {email});
+    if (result.length === 0) return null;
+    return result[0] ?? null;
   }
   async findById(id: number): Promise<IAccountRow | null> {
-    const [[result]] = await this.pool.query<IAccountRow[]>('SELECT * FROM accounts WHERE id=:id LIMIT 1', { id });
-    if (!result) return null;
-    return result;
+    const [result] = await this.pool.query<IAccountRow[]>('SELECT * FROM accounts WHERE id=:id LIMIT 1', {id});
+    if (result.length === 0) return null;
+    return result[0] ?? null;
   }
   async findByCriteria(firstString?: string, lastString?: string): Promise<IAccountRow[] | null> {
-    const query =
-      firstString && !lastString
-        ? 'SELECT CONCAT(firstName, " ", lastName) as fullName, id FROM accounts WHERE firstName=:firstString OR lastName=:firstString'
-        : 'SELECT CONCAT(firstName, " ", lastName) as fullName, id FROM accounts WHERE lastName=:lastString OR firstName=:lastString';
-    const [result] = await this.pool.query<IAccountRow[]>(query, { firstString, lastString });
-    if (!result) return null;
-    return result;
+
+    let query: string | undefined = undefined;
+    if (firstString && lastString) {
+      query = 'SELECT CONCAT(firstName," ", lastName) as fullName, id FROM accounts WHERE ((firstName=:firstString AND lastName=:lastString) OR (lastName=:firstString AND firstName=:lastString))';
+    } else if (firstString && !lastString) {
+      query = 'SELECT CONCAT(firstName, " ", lastName) as fullName, id FROM accounts WHERE firstName=:firstString OR lastName=:firstString';
+    } else if (lastString && !firstString) {
+      query = 'SELECT CONCAT(firstName, " ", lastName) as fullName, id FROM accounts WHERE lastName=:lastString OR firstName=:lastString';
+    }
+    if (query) {
+      const [result] = await this.pool.query<IAccountRow[]>(query, {firstString, lastString});
+      if (result.length === 0) return null;
+      return result;
+    } else {return null;}
+
   }
   async isExist(email: string): Promise<boolean> {
-    const [[result]] = await this.pool.query<RowDataPacket[]>('SELECT 1 FROM accounts WHERE email=:email', { email });
-    return !!result;
+    const [result] = await this.pool.query<RowDataPacket[]>('SELECT 1 FROM accounts WHERE email=:email', {email});
+    return result.length > 0;
   }
   async insert(data: IAccountInsertDTO): Promise<boolean> {
     const [result] = await this.pool.execute<ResultSetHeader>(
       'INSERT INTO accounts (email,password,birthdayDate,firstName,lastName) VALUES (:email,:password,:birthdayDate,:firstName,:lastName)',
-      { ...data },
+      {...data},
     );
     return result.affectedRows > 0;
   }
@@ -55,7 +63,7 @@ export class AccountRepository implements IAccountRepository {
     return result.affectedRows > 0;
   }
   async delete(email: string): Promise<boolean> {
-    const [result] = await this.pool.execute<ResultSetHeader>('DELETE FROM accounts WHERE email=:email', { email: email });
+    const [result] = await this.pool.execute<ResultSetHeader>('DELETE FROM accounts WHERE email=:email', {email: email});
     return result.affectedRows > 0;
   }
 }
